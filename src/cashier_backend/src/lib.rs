fedadf6a7afbf7fc4b6b3ef39ade1457a773237f
Long Tran <57053685+baolongt@users.noreply.github.com>
2024-09-04 10:57:21 +0700

    init

--- src/cashier_backend/src/lib.rs
+++ src/cashier_backend/src/lib.rs
@@ -1,4 +1,13 @@
 #[ic_cdk::query]
 fn greet(name: String) -> String {
+
+
+
+    
     format!("Hello, {}!", name)
+}
+
+#[ic_cdk::query]
+fn test() -> String {
+    "Hello, World!".to_string()
 }
---
+++ src/cashier_frontend/src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+    return <main>init</main>;
+}
+
+export default App;
