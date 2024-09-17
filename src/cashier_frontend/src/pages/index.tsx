a3a7cd117a42b19246ce746d746894b88a421d87
Phu Nguyen <nnphongphu@gmail.com>
2024-09-17 16:49:30 +0700

    fix: login

--- src/cashier_frontend/src/pages/index.tsx
+++ src/cashier_frontend/src/pages/index.tsx
@@ -5,42 +5,55 @@ import { ConnectWallet } from "@nfid/identitykit/react";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { HttpAgent } from "@dfinity/agent";
+import { BACKEND_CANISTER_ID } from "@/const";
+import { parseResultResponse } from "@/utils";
 
 export default function HomePage() {
     const { agent, identity } = useIdentityKit();
     const [links, setLinks] = useState<any>([]);
     const [user, setUser] = useState<any>(null);
 
-    const actor = createActor("txyno-ch777-77776-aaaaq-cai", {
-        agent: HttpAgent.createSync({ identity }),
+    const actor = createActor(BACKEND_CANISTER_ID, {
+        agent: HttpAgent.createSync({ identity, host: "https://icp0.io" }),
     });
 
     useEffect(() => {
+        if (!agent) return;
         const createUser = async () => {
             const user = await actor.get_user();
             if ("Ok" in user) setUser(user);
             else {
-                const create = await actor.create_user();
-                if ("Ok" in create) setUser(create.Ok);
+                const user = parseResultResponse(await actor.create_user());
+                setUser(user);
             }
         };
         createUser();
     }, [agent]);
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         const links = await actor.get_links([{
-    //             offset: BigInt(0),
-    //             limit: BigInt(10)
-    //         }]);
-    //         console.log("links", links);
-    //         if ('Ok' in links) setLinks(links!.Ok.data);
-    //     }
-    //     fetchData();
-    // }, [user]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const links = parseResultResponse(
+                await actor.get_links([
+                    {
+                        offset: BigInt(0),
+                        limit: BigInt(10),
+                    },
+                ]),
+            );
+            console.log(links.data);
+            setLinks(links.data);
+        };
+        fetchData();
+    }, [user]);
 
-    // console.log(agent);
-    console.log("user", user);
+    const handleCreateLink = async () => {
+        const link = parseResultResponse(
+            await actor.create_link({
+                link_type: { NftCreateAndAirdrop: null },
+            }),
+        );
+        console.log("Link created", link);
+    };
 
     return (
         <div className="w-screen flex justify-center py-5">
@@ -59,9 +72,18 @@ export default function HomePage() {
                 <div className="w-full flex justify-between items-center">
                     <h2 className="text-base font-semibold">Links created by me</h2>
                     <Link to="/create">
-                        <Button>+</Button>
+                        <Button onClick={handleCreateLink}>+</Button>
                     </Link>
                 </div>
+                {links.map((link: any) => (
+                    <div
+                        key={link.id}
+                        className="w-full my-3 p-3 border border-gray-200 rounded-md"
+                    >
+                        <h3 className="text-base font-semibold">Link id: {link.id}</h3>
+                        <p className="text-sm text-gray-500">{"No title"}</p>
+                    </div>
+                ))}
             </div>
         </div>
     );
