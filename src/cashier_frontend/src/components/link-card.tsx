0a335ae94a9f4287623b1500a949cfaadee6f008
lehoangkhoi01 <khoihle01012001@gmail.com>
2024-10-10 17:09:12 +0700

    FE- Update link template and preview size portion

--- src/cashier_frontend/src/components/link-card.tsx
+++ src/cashier_frontend/src/components/link-card.tsx
@@ -16,28 +16,25 @@ export default function LinkCard({
     onClaim?: () => void;
 }) {
     return (
-        <div className="flex flex-col items-center bg-lightgreen rounded-md mt-5 py-5 px-8 h-[70vh]">
+        <div className="flex flex-col items-center bg-lightgreen rounded-md py-5 mt-5 h-[60vh]">
             <div className="text-lg font-medium">{header}</div>
             <div
                 id="phone-frame"
-                className="flex flex-col items-center bg-white rounded-[2rem] border-black border-8 mt-3 px-3 h-screen"
+                className="flex flex-col items-center bg-white rounded-[2rem] border-black border-8 mt-3 px-3 h-[100vw] w-[50vw] md:w-[70%]"
             >
                 <div
                     id="phone-notch-section"
-                    className="flex w-3/5 h-7 bg-black items-center border-black border-8 rounded-b-2xl"
+                    className="flex w-3/5 h-3 md:h-7 bg-black items-center border-black border-8 rounded-b-2xl"
                 ></div>
                 <div className="w-full flex justify-center items-center mt-3">
                     <img src="./logo.svg" alt="Cashier logo" className="max-w-[80px]" />
                 </div>
                 <div className="flex flex-col items-center bg-lightgreen rounded-md mt-3 p-3">
                     <img src={src} alt="Link template" />
-                    <h3 className="font-semibold py-2">{title}</h3>
-                    <h3 className="text-xs">{message}</h3>
+                    <h3 className="font-semibold py-2 text-xs">{title}</h3>
+                    <h3 className="text-[0.5rem]">{message}</h3>
                     <div
-                        className={cn(
-                            "text-white bg-green rounded-full py-1 px-8 mt-3 text-base w-full text-center",
-                            onClaim ? "cursor-pointer" : "",
-                        )}
+                        className="text-white bg-green rounded-full py-1 mt-3 text-xs w-[100%] text-center"
                         onClick={onClaim}
                     >
                         {label}
