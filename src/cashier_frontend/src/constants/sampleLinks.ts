1f10417e3cd6c3ba382cf0263949b19a921628f0
Hoang Khoi <59761477+lehoangkhoi01@users.noreply.github.com>
2024-10-12 22:45:20 +0700

    Use async function to allow copy url to clipboard (#28)

---
+++ src/cashier_frontend/src/constants/sampleLinks.ts
@@ -0,0 +1,31 @@
+import { LinkDetail } from "../../../declarations/cashier_backend/cashier_backend.did";
+
+export const sampleLink1: LinkDetail = {
+    id: "",
+    title: ["Special moments"],
+    image: [""],
+    link_type: [{ NftCreateAndAirdrop: null }],
+    description: [
+        "I wanted to capture this special moment forever. And I’d like to share it with my closest of friends.",
+    ],
+    template: [],
+    creator: [],
+    actions: [],
+    asset_info: [],
+    state: [],
+};
+
+export const sampleLink2: LinkDetail = {
+    id: "",
+    title: ["Special moments"],
+    image: [""],
+    link_type: [{ NftCreateAndAirdrop: null }],
+    description: [
+        "I wanted to capture this special moment forever. And I’d like to share it with my closest of friends.",
+    ],
+    template: [],
+    creator: [],
+    actions: [],
+    asset_info: [],
+    state: [],
+};
