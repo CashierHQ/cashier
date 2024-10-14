06369f9b857a8cc0b86d53d64ba80560859fc9a3
lehoangkhoi01 <khoihle01012001@gmail.com>
2024-10-15 00:05:13 +0700

    FE - Add 2 sample links for new user

--- src/cashier_frontend/src/constants/sampleLinks.ts
+++ src/cashier_frontend/src/constants/sampleLinks.ts
@@ -1,31 +1,25 @@
+import { Chain, State, Template, UpdateLinkInput } from "@/services/types/link.service.types";
 import { LinkDetail } from "../../../declarations/cashier_backend/cashier_backend.did";
+import { base64SampleLinkImage1, base64SampleLinkImage2 } from "./base64Images";
 
-export const sampleLink1: LinkDetail = {
-    id: "",
-    title: ["Special moments"],
-    image: [""],
-    link_type: [{ NftCreateAndAirdrop: null }],
-    description: [
+export const sampleLink1: UpdateLinkInput = {
+    title: "Special moments",
+    image: base64SampleLinkImage1,
+    description:
         "I wanted to capture this special moment forever. And I’d like to share it with my closest of friends.",
-    ],
-    template: [],
-    creator: [],
-    actions: [],
-    asset_info: [],
-    state: [],
+    amount: 10,
+    chain: Chain.IC,
+    state: State.PendingPreview,
+    template: Template.Left,
 };
 
-export const sampleLink2: LinkDetail = {
-    id: "",
-    title: ["Special moments"],
-    image: [""],
-    link_type: [{ NftCreateAndAirdrop: null }],
-    description: [
-        "I wanted to capture this special moment forever. And I’d like to share it with my closest of friends.",
-    ],
-    template: [],
-    creator: [],
-    actions: [],
-    asset_info: [],
-    state: [],
+export const sampleLink2: UpdateLinkInput = {
+    title: "Proof of attendance",
+    image: base64SampleLinkImage2,
+    description:
+        "Thank you for attending our coffee brewing workshop. Here is an NFT as a proof of your attendance.",
+    amount: 20,
+    chain: Chain.IC,
+    state: State.PendingPreview,
+    template: Template.Left,
 };
