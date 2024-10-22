f17a8665e51c88336960aedf50d6b6fd40525f97
Long Tran <57053685+baolongt@users.noreply.github.com>
2024-10-22 23:20:07 +0700

    add icrc28 icrc21 implement for backend (#50)

--- src/cashier_backend/src/core/mod.rs
+++ src/cashier_backend/src/core/mod.rs
@@ -1,11 +1,13 @@
 use crate::core::link::types::*;
 use crate::types::api::*;
 use crate::types::error::*;
+use crate::types::icrc::*;
 use crate::types::link_detail::*;
 use crate::types::user::*;
 
 pub mod claim;
 pub mod guard;
+pub mod icrc;
 pub mod init_and_upgrade;
 pub mod link;
 pub mod user;
