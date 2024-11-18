d0678bc0dfd6f19dd30859a2b206dd8f8e03b9ec
hardworking-toptal-dev <161673729+hardworking-toptal-dev@users.noreply.github.com>
2024-11-18 01:16:04 -0500

    refactor: types for state-machine

--- src/cashier_backend/src/core/link/types.rs
+++ src/cashier_backend/src/core/link/types.rs
@@ -7,27 +7,39 @@ use crate::core::{Action, AssetAirdropInfo, LinkDetailUpdate, LinkType, State, T
 pub struct CreateLinkInput {
     pub link_type: LinkType,
 }
-
 #[derive(Serialize, Deserialize, Debug, CandidType)]
-pub struct UpdateLinkInput {
+pub struct UpdateLinkParams {
     pub title: Option<String>,
     pub description: Option<String>,
     pub image: Option<String>,
     pub asset_info: Option<AssetAirdropInfo>,
     pub actions: Option<Vec<Action>>,
     pub template: Option<Template>,
+}
+
+#[derive(Serialize, Deserialize, Debug, CandidType)]
+pub enum LinkStateMachineAction {
+    Continue,
+    Back,
+}
+
+#[derive(Serialize, Deserialize, Debug, CandidType)]
+pub struct UpdateLinkInput {
+    pub id: String,
+    pub params: UpdateLinkParams,
+    pub action: LinkStateMachineAction,
     pub state: Option<State>,
 }
 
 impl UpdateLinkInput {
     pub fn to_link_detail_update(&self) -> LinkDetailUpdate {
         LinkDetailUpdate {
-            title: self.title.clone(),
-            description: self.description.clone(),
-            image: self.image.clone(),
-            asset_info: self.asset_info.clone(),
-            actions: self.actions.clone(),
-            template: self.template.clone(),
+            title: self.params.title.clone(),
+            description: self.params.description.clone(),
+            image: self.params.image.clone(),
+            asset_info: self.params.asset_info.clone(),
+            actions: self.params.actions.clone(),
+            template: self.params.template.clone(),
             state: self.state.clone(),
         }
     }
