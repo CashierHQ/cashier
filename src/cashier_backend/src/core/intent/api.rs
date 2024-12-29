7906f13e58b7bc6a017cbe8ab030e06b7ef727d8
Long Tran <57053685+baolongt@users.noreply.github.com>
2024-12-29 16:27:44 +0700

    add get consent message (#119)

--- src/cashier_backend/src/core/intent/api.rs
+++ src/cashier_backend/src/core/intent/api.rs
@@ -1,8 +1,9 @@
-use ic_cdk::update;
+use ic_cdk::{query, update};
 
 use crate::{
     core::{
-        guard::is_not_anonymous, CanisterError, CreateIntentConsentResponse, UpdateIntentInput,
+        guard::is_not_anonymous, CanisterError, CreateIntentConsent, CreateIntentConsentResponse,
+        GetConsentMessageInput, UpdateIntentInput,
     },
     services,
 };
@@ -14,8 +15,15 @@ pub async fn create_intent(
     input: CreateIntentInput,
 ) -> Result<CreateIntentConsentResponse, CanisterError> {
     // inside already check caller is creator
-    services::transaction::create::create(input).await
+    services::transaction::create::create_create_link_intent(input).await
 }
 
 #[update(guard = "is_not_anonymous")]
 pub async fn update_intent(input: UpdateIntentInput) {}
+
+#[query(guard = "is_not_anonymous")]
+pub async fn get_consent_message(
+    input: GetConsentMessageInput,
+) -> Result<CreateIntentConsent, CanisterError> {
+    services::transaction::consent_message::get_consent_message(input)
+}
