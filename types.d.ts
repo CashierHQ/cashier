3b2bbc7f32450f998c41e7574ffc2222cf9db7c8
lehoangkhoi01 <khoihle01012001@gmail.com>
2025-01-20 00:57:26 +0700

    Update

--- types.d.ts
+++ types.d.ts
@@ -1,16 +1,11 @@
 /* eslint-disable no-var */
 import { PocketIcServer } from "@hadronous/pic";
-import { Buffer as ImportedBuffer } from "buffer";
 
 declare global {
     declare var __PIC__: PocketIcServer;
-    var Buffer: typeof import("buffer").Buffer;
-
     namespace NodeJS {
         interface ProcessEnv {
             PIC_URL: string;
         }
     }
 }
-
-globalThis.Buffer = globalThis.Buffer || ImportedBuffer;
