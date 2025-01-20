76a61dbde7414ce431948f66c19f893d0f4179f6
Hoang Khoi <59761477+lehoangkhoi01@users.noreply.github.com>
2025-01-20 12:04:03 +0700

    FE - Integrate the confirm transactions process (#152)
    * Update call canister service
    
    * Re-fetching to get link data
    
    * Update link to active when all the trans success
    
    * Update
    
    * Update
    
    * Add test form
    
    * Update to call update intent after call ICRC X success
    
    * Refractor naming
    
    * Group network fees by token address
    
    * Update

---
+++ src/cashier_frontend/src/components/test-form/user-wallet-test-form.tsx
@@ -0,0 +1,90 @@
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import CanisterUtilsService from "@/services/canisterUtils.service";
+import { useIdentity } from "@nfid/identitykit/react";
+import { TokenUtilService } from "@/services/tokenUtils.service";
+interface TestFormProps {
+    onCancel?: () => void;
+}
+export function UserWalletTestForm(props: TestFormProps) {
+    const [walletAddress, setWalletAddress] = React.useState("");
+    const [canisterId, setCanisterId] = React.useState("");
+    const [balance, setBalance] = React.useState(0);
+    const identity = useIdentity();
+    const onLinkIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setWalletAddress(e.target.value);
+    };
+    const onCanisterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCanisterId(e.target.value);
+    };
+    const handleSubmitForm = async () => {
+        try {
+            const canisterUtils = new CanisterUtilsService(identity);
+            const fetchedBalance = await canisterUtils.checkAccountBalance(
+                canisterId,
+                walletAddress,
+            );
+            if (fetchedBalance) {
+                const parsedAmount = await TokenUtilService.getHumanReadableAmount(
+                    fetchedBalance,
+                    canisterId,
+                );
+                setBalance(parsedAmount);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    return (
+        <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>User wallet</CardTitle>
+                <CardDescription>
+                    Test user wallet with wallet address and token address
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="link">Identity</Label>
+                            <Input
+                                id="link"
+                                placeholder="Identity"
+                                value={walletAddress}
+                                onChange={onLinkIdChange}
+                            />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="link">Canister</Label>
+                            <Input
+                                id="canister"
+                                placeholder="Canister ID"
+                                value={canisterId}
+                                onChange={onCanisterChange}
+                            />
+                        </div>
+                    </div>
+                </form>
+                <div className="mt-3">Balance: {balance}</div>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <Button variant="outline" onClick={props.onCancel}>
+                    Cancel
+                </Button>
+                <Button onClick={handleSubmitForm}>Query</Button>
+            </CardFooter>
+        </Card>
+    );
+}
--- src/cashier_frontend/src/utils/index.ts
+++ src/cashier_frontend/src/utils/index.ts
@@ -23,7 +23,6 @@ export type Response<T, E> =
       };
 
 export const parseResultResponse = <T, E>(response: Response<T, E>): T => {
-    console.log("response", response);
     if ("ok" in response) {
         return response.ok;
     } else if ("Ok" in response) {
