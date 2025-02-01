a8eeff1a202fe46c768025940c1c68f5f1a06643
cryptokovalyshyn <serhiikovalyshyn1@gmail.com>
2025-02-01 22:39:39 +0200

    updated asset ui components

--- src/cashier_frontend/src/components/ui/asset.tsx
+++ src/cashier_frontend/src/components/ui/asset.tsx
@@ -1,23 +1,30 @@
 import { FC } from "react";
-import { AssetAvatar } from "../confirmation-popup/asset-avatar";
+import { AssetAvatar } from "@/components/ui/asset-avatar";
+import { Spinner } from "@/components/ui/spinner";
 
 export type AssetProps = {
     title?: string | undefined;
     src?: string | undefined;
     symbol?: string | undefined;
     amount?: number | undefined;
+
+    isLoading?: boolean;
 };
 
-export const Asset: FC<AssetProps> = ({ title, src, symbol, amount }) => {
+export const Asset: FC<AssetProps> = ({ title, src, symbol, amount, isLoading }) => {
     return (
         <div className="flex justify-between items-center">
             <h6>{title}</h6>
 
-            <div className="flex items-center">
-                <span>{amount}</span>
-                <span className="ml-1">{symbol}</span>
-                <AssetAvatar className="ml-1" src={src} symbol={symbol} />
-            </div>
+            {isLoading ? (
+                <Spinner width={22} />
+            ) : (
+                <div className="flex items-center">
+                    <span>{amount}</span>
+                    <span className="ml-1">{symbol}</span>
+                    <AssetAvatar className="ml-1" src={src} symbol={symbol} />
+                </div>
+            )}
         </div>
     );
 };
