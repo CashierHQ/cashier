e2eb9d61ad602efcc796694aa1142979742c7392
cryptokovalyshyn <serhiikovalyshyn1@gmail.com>
2025-02-06 21:25:09 +0200

    created usd-switch ui component

---
+++ src/cashier_frontend/src/components/link-details/usd-switch.tsx
@@ -0,0 +1,32 @@
+import { ArrowUpDown } from "lucide-react";
+import { FC } from "react";
+
+type UsdSwitchProps = {
+    amount: number | undefined;
+    symbol: string | undefined;
+    amountUsd: number | undefined;
+    isUsd: boolean;
+    onToggle: (isUsd: boolean) => void;
+};
+
+export const UsdSwitch: FC<UsdSwitchProps> = ({ isUsd, onToggle, amount, symbol, amountUsd }) => {
+    return (
+        <button
+            type="button"
+            className="flex text-destructive items-center"
+            onClick={() => onToggle(!isUsd)}
+        >
+            <span className="text-sm leading-none">
+                {isUsd ? (
+                    <>
+                        {amount} {symbol}
+                    </>
+                ) : (
+                    <>{amountUsd} USD</>
+                )}
+            </span>
+
+            <ArrowUpDown className="ml-1" size={16} />
+        </button>
+    );
+};
