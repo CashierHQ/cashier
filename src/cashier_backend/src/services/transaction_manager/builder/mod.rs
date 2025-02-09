53be9d050c557cbfb690a0362d979406e7450436
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-02-10 00:17:54 +0700

    add builder for icrc-112

--- src/cashier_backend/src/services/transaction_manager/builder/mod.rs
+++ src/cashier_backend/src/services/transaction_manager/builder/mod.rs
@@ -1,13 +1,10 @@
 // pub mod approve_cashier_fee;
+pub mod icrc112;
 pub mod intent;
 // pub mod transfer_to_link_escrow_wallet;
 // pub mod update_intent;
 
 use cashier_types::Intent;
-
-pub trait TransactionBuilder {
-    fn build(&self) -> ();
-}
 
 pub struct IntentBuildResponse {
     pub intents: Vec<Intent>,
