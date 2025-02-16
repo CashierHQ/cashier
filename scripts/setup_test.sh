de8007df4d7cf846b307716957a949ea5212560f
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-02-16 14:36:47 +0700

    fix bug

--- scripts/setup_test.sh
+++ scripts/setup_test.sh
@@ -1,8 +1,15 @@
-#compress the backend wasm file
+#!/bin/bash
+
+# filepath: /Users/longtran/Documents/code/cashier/cashier/scripts/setup_test.sh
+
+# Create the artifacts directory if it doesn't exist
+mkdir -p artifacts
+
+# Compress the backend wasm file
 gzip -c target/wasm32-unknown-unknown/release/cashier_backend.wasm > artifacts/cashier_backend.wasm.gz
 
-#download token canister wasm file
+# Download token canister wasm file
 curl -o artifacts/token_canister.wasm.gz https://download.dfinity.systems/ic/d4c3bb26c207e020c49f3aafe11bd77e6a75e85d/canisters/ledger-canister.wasm.gz
 
-#download token did file
+# Download token did file
 curl -o artifacts/token.did https://raw.githubusercontent.com/dfinity/ic/d4c3bb26c207e020c49f3aafe11bd77e6a75e85d/rs/rosetta-api/icp_ledger/ledger.did
