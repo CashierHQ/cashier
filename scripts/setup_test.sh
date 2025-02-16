2cca9f1eb7b4918730b1444bd935df3a058a176c
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-02-16 15:34:37 +0700

    fix

--- scripts/setup_test.sh
+++ scripts/setup_test.sh
@@ -16,6 +16,7 @@ curl -o artifacts/token_canister.wasm.gz https://download.dfinity.systems/ic/d4c
 # Download token did file
 curl -o artifacts/token.did https://raw.githubusercontent.com/dfinity/ic/d4c3bb26c207e020c49f3aafe11bd77e6a75e85d/rs/rosetta-api/icp_ledger/ledger.did
 
+dfx canister create icp_ledger_canister
 dfx build icp_ledger_canister
 
 dfx generate cashier_backend
