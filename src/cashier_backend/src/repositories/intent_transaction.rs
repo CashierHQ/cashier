8245495a09e737b8aa5052febd7fa1d9de8f95eb
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-02-21 16:43:36 +0700

    Be/add-unit-test (#189)
    * Change icrc name
    
    * Update validation for icrc112 execute
    
    * Separate ICRC-112 logic to different service
    
    * Clean up
    
    * Update type for nonce
    
    * add unit test
    
    * Add jest to front-end src
    
    * Add unit test
    
    * Add handling 1001 error
    
    * Update unit test
    
    * Initial setup
    
    * Initialize signer
    
    * Fix conflict
    
    * Add icrc 112 logic to channel
    
    * Cherry-pick commit 7393cfbe557b661044f045f3b4cf0a4fb09d6cec for src/repositories/
    
    * adjust new repository
    
    * remove un-use macro
    
    * add unit test
    
    * add faux
    
    * Revert "remove un-use macro"
    
    This reverts commit faef2f8c2fbff729ea642833d312e5aae2711d27.
    
    * add some test
    
    * [DRAFT] Set up signer (#187)
    
    * Change icrc name
    
    * Update validation for icrc112 execute
    
    * Separate ICRC-112 logic to different service
    
    * Clean up
    
    * Update type for nonce
    
    * add unit test
    
    * Add jest to front-end src
    
    * Add unit test
    
    * Add handling 1001 error
    
    * Update unit test
    
    * Initial setup
    
    * Initialize signer
    
    * Fix conflict
    
    * Add icrc 112 logic to channel
    
    * Fix conflict
    
    * Add agent to
    
    * Rename
    
    * Remove unused code
    
    * add icrc-112
    
    * change nonce to string
    
    * ADJUST call signer service
    
    * add type for parseResponse
    
    ---------
    
    Co-authored-by: Long Tran <57053685+baolongt@users.noreply.github.com>
    
    * update
    
    * remove test
    
    ---------
    
    Co-authored-by: lehoangkhoi01 <khoihle01012001@gmail.com>
    Co-authored-by: Hoang Khoi <59761477+lehoangkhoi01@users.noreply.github.com>

--- src/cashier_backend/src/repositories/intent_transaction.rs
+++ src/cashier_backend/src/repositories/intent_transaction.rs
@@ -1,23 +1,17 @@
+use super::INTENT_TRANSACTION_STORE;
 use cashier_types::{IntentTransaction, IntentTransactionKey};
 
-use super::INTENT_TRANSACTION_STORE;
+#[cfg_attr(test, faux::create)]
+pub struct IntentTransactionRepository {}
 
-pub fn create(intent_transaction: IntentTransaction) -> IntentTransaction {
-    INTENT_TRANSACTION_STORE.with_borrow_mut(|store| {
-        let key = IntentTransactionKey {
-            intent_id: intent_transaction.intent_id.clone(),
-            transaction_id: intent_transaction.transaction_id.clone(),
-        };
+#[cfg_attr(test, faux::methods)]
+impl IntentTransactionRepository {
+    pub fn new() -> Self {
+        Self {}
+    }
 
-        store.insert(key.to_str(), intent_transaction.clone());
-        store.insert(key.to_str_reverse(), intent_transaction.clone());
-        intent_transaction
-    })
-}
-
-pub fn batch_create(intent_transactions: Vec<IntentTransaction>) {
-    INTENT_TRANSACTION_STORE.with_borrow_mut(|store| {
-        for intent_transaction in intent_transactions {
+    pub fn create(&self, intent_transaction: IntentTransaction) -> IntentTransaction {
+        INTENT_TRANSACTION_STORE.with_borrow_mut(|store| {
             let key = IntentTransactionKey {
                 intent_id: intent_transaction.intent_id.clone(),
                 transaction_id: intent_transaction.transaction_id.clone(),
@@ -25,48 +19,63 @@ pub fn batch_create(intent_transactions: Vec<IntentTransaction>) {
 
             store.insert(key.to_str(), intent_transaction.clone());
             store.insert(key.to_str_reverse(), intent_transaction.clone());
-        }
-    });
-}
+            intent_transaction
+        })
+    }
 
-pub fn get(intent_id: String, transaction_id: String) -> Option<IntentTransaction> {
-    let id = IntentTransactionKey {
-        intent_id: intent_id.clone(),
-        transaction_id: transaction_id.clone(),
-    };
-    INTENT_TRANSACTION_STORE.with_borrow(|store| store.get(&id.to_str()).clone())
-}
+    pub fn batch_create(&self, intent_transactions: Vec<IntentTransaction>) {
+        INTENT_TRANSACTION_STORE.with_borrow_mut(|store| {
+            for intent_transaction in intent_transactions {
+                let key = IntentTransactionKey {
+                    intent_id: intent_transaction.intent_id.clone(),
+                    transaction_id: intent_transaction.transaction_id.clone(),
+                };
 
-pub fn get_by_intent_id(intent_id: String) -> Vec<IntentTransaction> {
-    INTENT_TRANSACTION_STORE.with_borrow(|store| {
-        let key = IntentTransactionKey {
+                store.insert(key.to_str(), intent_transaction.clone());
+                store.insert(key.to_str_reverse(), intent_transaction.clone());
+            }
+        });
+    }
+
+    pub fn get(&self, intent_id: String, transaction_id: String) -> Option<IntentTransaction> {
+        let id = IntentTransactionKey {
             intent_id: intent_id.clone(),
-            transaction_id: "".to_string(),
-        };
-
-        let prefix = key.to_str();
-
-        store
-            .range(prefix.clone()..)
-            .filter(|(key, _)| key.starts_with(&prefix))
-            .map(|(_, value)| value.clone())
-            .collect()
-    })
-}
-
-pub fn get_by_transaction_id(transaction_id: String) -> Vec<IntentTransaction> {
-    INTENT_TRANSACTION_STORE.with_borrow(|store| {
-        let key = IntentTransactionKey {
-            intent_id: "".to_string(),
             transaction_id: transaction_id.clone(),
         };
+        INTENT_TRANSACTION_STORE.with_borrow(|store| store.get(&id.to_str()).clone())
+    }
 
-        let prefix = key.to_str_reverse();
+    pub fn get_by_intent_id(&self, intent_id: String) -> Vec<IntentTransaction> {
+        INTENT_TRANSACTION_STORE.with_borrow(|store| {
+            let key = IntentTransactionKey {
+                intent_id: intent_id.clone(),
+                transaction_id: "".to_string(),
+            };
 
-        store
-            .range(prefix.clone()..)
-            .filter(|(key, _)| key.starts_with(&prefix))
-            .map(|(_, value)| value.clone())
-            .collect()
-    })
+            let prefix = key.to_str();
+
+            store
+                .range(prefix.clone()..)
+                .filter(|(key, _)| key.starts_with(&prefix))
+                .map(|(_, value)| value.clone())
+                .collect()
+        })
+    }
+
+    pub fn get_by_transaction_id(&self, transaction_id: String) -> Vec<IntentTransaction> {
+        INTENT_TRANSACTION_STORE.with_borrow(|store| {
+            let key = IntentTransactionKey {
+                intent_id: "".to_string(),
+                transaction_id: transaction_id.clone(),
+            };
+
+            let prefix = key.to_str_reverse();
+
+            store
+                .range(prefix.clone()..)
+                .filter(|(key, _)| key.starts_with(&prefix))
+                .map(|(_, value)| value.clone())
+                .collect()
+        })
+    }
 }
--- src/cashier_backend/src/services/link/mod.rs
+++ src/cashier_backend/src/services/link/mod.rs
@@ -8,13 +8,16 @@ pub mod validate_active_link;
 
 use crate::{
     core::link::types::CreateLinkInput,
-    repositories::{self, action, link, link_action, user_link, user_wallet},
+    repositories::{self, action, link_action, user_link, user_wallet},
     types::api::{PaginateInput, PaginateResult},
     warn,
 };
 
 pub fn create_new(creator: String, input: CreateLinkInput) -> Result<String, String> {
-    let user_wallet = user_wallet::get(&creator).ok_or_else(|| "User not found".to_string())?;
+    let user_wallet_repository = user_wallet::UserWalletRepository::new();
+    let user_wallet = user_wallet_repository
+        .get(&creator)
+        .ok_or_else(|| "User not found".to_string())?;
 
     let user_id = user_wallet.user_id;
 
@@ -42,8 +45,11 @@ pub fn create_new(creator: String, input: CreateLinkInput) -> Result<String, Str
         link_id: link_id_str.clone(),
     };
 
-    link::create(new_link);
-    user_link::create(new_user_link);
+    let link_repository = repositories::link::LinkRepository::new();
+    let user_link_repository = user_link::UserLinkRepository::new();
+
+    link_repository.create(new_link);
+    user_link_repository.create(new_user_link);
 
     Ok(link_id_str)
 }
@@ -52,10 +58,10 @@ pub fn get_links_by_principal(
     principal: String,
     pagination: PaginateInput,
 ) -> Result<PaginateResult<Link>, String> {
-    let user_wallet = match user_wallet::get(&principal) {
-        Some(user_id) => user_id,
-        None => return Err("User not found".to_string()),
-    };
+    let user_wallet_repository = user_wallet::UserWalletRepository::new();
+    let user_wallet = user_wallet_repository
+        .get(&principal)
+        .ok_or_else(|| "User not found".to_string())?;
 
     let user_id = user_wallet.user_id;
 
@@ -71,7 +77,8 @@ pub fn get_links_by_user_id(
     user_id: String,
     pagination: PaginateInput,
 ) -> Result<PaginateResult<Link>, String> {
-    let user_links = user_link::get_links_by_user_id(user_id, pagination);
+    let user_link_repository = user_link::UserLinkRepository::new();
+    let user_links = user_link_repository.get_links_by_user_id(user_id, pagination);
 
     let link_ids = user_links
         .data
@@ -79,37 +86,41 @@ pub fn get_links_by_user_id(
         .map(|link_user| link_user.link_id.clone())
         .collect();
 
-    let links = link::get_batch(link_ids);
+    let link_repository = repositories::link::LinkRepository::new();
+    let links = link_repository.get_batch(link_ids);
 
     let res = PaginateResult::new(links, user_links.metadata);
     Ok(res)
 }
 
 pub fn get_link_by_id(id: String) -> Result<Link, String> {
-    let link = match link::get(&id) {
-        Some(link) => link,
-        None => return Err("Link not found".to_string()),
-    };
+    let link_repository = repositories::link::LinkRepository::new();
+    let link = link_repository
+        .get(&id)
+        .ok_or_else(|| "Link not found".to_string())?;
 
-    return Ok(link);
+    Ok(link)
 }
 
 pub fn get_link_action(link_id: String, action_type: String) -> Option<Action> {
-    let link_actions = link_action::get_by_link_action(link_id, action_type);
+    let link_action_repository = link_action::LinkActionRepository::new();
+    let link_actions = link_action_repository.get_by_link_action(link_id, action_type);
 
-    if link_actions.len() == 0 {
+    if link_actions.is_empty() {
         return None;
     }
 
     let action_id = link_actions.first().unwrap().action_id.clone();
 
-    let action = action::get(action_id);
+    let action_repository = action::ActionRepository::new();
+    let action = action_repository.get(action_id);
 
-    return action;
+    action
 }
 
 pub fn is_link_creator(caller: String, link_id: &String) -> bool {
-    let user_wallet = match user_wallet::get(&caller) {
+    let user_wallet_repository = user_wallet::UserWalletRepository::new();
+    let user_wallet = match user_wallet_repository.get(&caller) {
         Some(u) => u,
         None => {
             warn!("User not found");
@@ -117,18 +128,14 @@ pub fn is_link_creator(caller: String, link_id: &String) -> bool {
         }
     };
 
-    match link::get(&link_id) {
-        None => {
-            return false;
-        }
-        Some(link_detail) => {
-            let creator = link_detail.creator.clone();
-
-            return creator == user_wallet.user_id;
-        }
-    };
+    let link_repository = repositories::link::LinkRepository::new();
+    match link_repository.get(&link_id) {
+        None => false,
+        Some(link_detail) => link_detail.creator == user_wallet.user_id,
+    }
 }
 
 pub fn is_link_exist(link_id: String) -> bool {
-    repositories::link::get(&link_id).is_some()
+    let link_repository = repositories::link::LinkRepository::new();
+    link_repository.get(&link_id).is_some()
 }
