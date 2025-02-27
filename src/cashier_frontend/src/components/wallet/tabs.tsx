0cf7a089456be436a338e98dfebb21388ecd5f1c
cryptokovalyshyn <serhiikovalyshyn1@gmail.com>
2025-02-27 16:31:46 +0200

    added temporary banner for nft

--- src/cashier_frontend/src/components/wallet/tabs.tsx
+++ src/cashier_frontend/src/components/wallet/tabs.tsx
@@ -2,6 +2,8 @@ import { useTranslation } from "react-i18next";
 import { WalletNftsTab } from "./nfts-tab";
 import { WalletTokensTab } from "./tokens-tab";
 import Tabs from "@/components/ui/tabs2";
+import { useState } from "react";
+import { Info } from "lucide-react";
 
 export enum WalletTab {
     Tokens = "Tokens",
@@ -11,22 +13,59 @@ export enum WalletTab {
 export function WalletTabs() {
     const { t } = useTranslation();
 
+    const [tab, setTab] = useState<WalletTab>(WalletTab.Tokens);
+
+    //TODO: make an actual toast
+    const [showToast, setShowToast] = useState<boolean>(false);
+
+    function handleTabChange(newTab: string) {
+        if (newTab === WalletTab.Nfts) {
+            setShowToast(true);
+
+            setTimeout(() => {
+                setShowToast(false);
+            }, 5000);
+        } else {
+            setTab(newTab as WalletTab);
+        }
+    }
+
     return (
-        <Tabs.Root defaultValue={WalletTab.Tokens} className="flex flex-col flex-grow">
-            <Tabs.List>
-                <Tabs.Trigger value={WalletTab.Tokens}>
-                    {t("wallet.tabs.tokens.header")}
-                </Tabs.Trigger>
-                <Tabs.Trigger value={WalletTab.Nfts}>{t("wallet.tabs.nfts.header")}</Tabs.Trigger>
-            </Tabs.List>
+        <>
+            <Tabs.Root
+                value={tab}
+                onValueChange={handleTabChange}
+                className="flex flex-col flex-grow"
+            >
+                <Tabs.List>
+                    <Tabs.Trigger value={WalletTab.Tokens}>
+                        {t("wallet.tabs.tokens.header")}
+                    </Tabs.Trigger>
+                    <Tabs.Trigger value={WalletTab.Nfts}>
+                        {t("wallet.tabs.nfts.header")}
+                    </Tabs.Trigger>
+                </Tabs.List>
 
-            <Tabs.Content value={WalletTab.Tokens}>
-                <WalletTokensTab />
-            </Tabs.Content>
+                <Tabs.Content value={WalletTab.Tokens}>
+                    <WalletTokensTab />
+                </Tabs.Content>
 
-            <Tabs.Content value={WalletTab.Nfts}>
-                <WalletNftsTab />
-            </Tabs.Content>
-        </Tabs.Root>
+                <Tabs.Content value={WalletTab.Nfts}>
+                    <WalletNftsTab />
+                </Tabs.Content>
+            </Tabs.Root>
+
+            {showToast && (
+                // TODO: Make an actual toast
+                <div className="flex gap-3 fixed bottom-9 left-5 right-5 p-4 bg-lightyellow rounded-xl">
+                    <Info className="fill-green stroke-lightyellow" size={32} />
+
+                    <div className="flex flex-col gap-1">
+                        <p className="text-sm font-semibold">{t("wallet.banner.title")}</p>
+                        <p className="text-grey text-sm">{t("wallet.banner.text")}</p>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
