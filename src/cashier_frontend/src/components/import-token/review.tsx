702602491ac16817ad34b89758f38ffec54a041e
cryptokovalyshyn <serhiikovalyshyn1@gmail.com>
2025-02-28 12:55:46 +0200

    fixed localization, added props to review

--- src/cashier_frontend/src/components/import-token/review.tsx
+++ src/cashier_frontend/src/components/import-token/review.tsx
@@ -1,11 +1,11 @@
 import { useTranslation } from "react-i18next";
 import { AssetAvatar } from "../ui/asset-avatar";
-import { IC_EXPLORER_IMAGES_PATH } from "@/services/icExplorer.service";
 import { Input } from "../ui/input";
 import { Message } from "../ui/message";
 import { Button } from "../ui/button";
-import { Chain } from "@/services/types/link.service.types";
 import { ImportTokenFormData } from "@/hooks/import-token.hooks";
+import { mapChainToLogo, mapChainToPrettyName } from "@/utils/map/chain.map";
+import { MOCK_TOKEN_DATA } from "@/constants/mock-data";
 
 interface ImportTokenReviewProps {
     data: ImportTokenFormData;
@@ -19,6 +19,8 @@ export function ImportTokenReview({ data, onImport = () => {} }: ImportTokenRevi
         onImport(data);
     }
 
+    const token = MOCK_TOKEN_DATA;
+
     return (
         <div className="flex flex-col flex-grow pt-6 pb-2">
             <div className="flex flex-col gap-6 flex-grow">
@@ -28,12 +30,12 @@ export function ImportTokenReview({ data, onImport = () => {} }: ImportTokenRevi
                     <div className="flex mt-2.5 gap-4">
                         <AssetAvatar
                             className="w-12 h-12 ml-0"
-                            src={`${IC_EXPLORER_IMAGES_PATH}mxzaz-hqaaa-aaaar-qaada-cai`}
-                            symbol="ckBTC"
+                            src={token.logo}
+                            symbol={token.symbol}
                         />
                         <div>
-                            <p>Kinic</p>
-                            <p className="text-grey">KINIC</p>
+                            <p>{token.name}</p>
+                            <p className="text-grey">{token.symbol}</p>
                         </div>
                     </div>
                 </div>
@@ -44,13 +46,13 @@ export function ImportTokenReview({ data, onImport = () => {} }: ImportTokenRevi
                     <div className="relative mt-2.5">
                         <Input
                             disabled
-                            value="Internet Computer"
+                            value={mapChainToPrettyName(token.chain)}
                             className="pl-12 disabled:opacity-100"
                         />
                         <AssetAvatar
                             className="w-6 h-6 absolute top-1/2 -translate-y-1/2"
-                            src={`${IC_EXPLORER_IMAGES_PATH}ryjl3-tyaaa-aaaaa-aaaba-cai`}
-                            symbol={Chain.IC}
+                            src={mapChainToLogo(token.chain)}
+                            symbol={token.chain}
                         />
                     </div>
                 </div>
@@ -58,13 +60,9 @@ export function ImportTokenReview({ data, onImport = () => {} }: ImportTokenRevi
                 <div>
                     <h2 className="font-semibold">{t("import.form.ledgerCanisterId.label")}</h2>
 
-                    <Input
-                        disabled
-                        value="73mez-iiaaa-aaaaq-aaasq-cai"
-                        className="mt-2.5 disabled:opacity-100"
-                    />
+                    <Input disabled value={token.address} className="mt-2.5 disabled:opacity-100" />
 
-                    <Message className="mt-2.5">{t("review.message")}</Message>
+                    <Message className="mt-3">{t("review.message")}</Message>
                 </div>
             </div>
 
--- src/cashier_frontend/src/utils/map/chain.map.ts
+++ src/cashier_frontend/src/utils/map/chain.map.ts
@@ -1,3 +1,4 @@
+import { IC_EXPLORER_IMAGES_PATH } from "@/services/icExplorer.service";
 import { Chain } from "@/services/types/link.service.types";
 
 const chainNameMap: { [key in Chain]: string } = {
@@ -6,4 +7,12 @@ const chainNameMap: { [key in Chain]: string } = {
 
 export function mapChainToPrettyName(chain: Chain): string {
     return chainNameMap[chain];
+}
+
+const chainLogoMap: { [key in Chain]: string } = {
+    [Chain.IC]: `${IC_EXPLORER_IMAGES_PATH}ryjl3-tyaaa-aaaaa-aaaba-cai`,
+};
+
+export function mapChainToLogo(chain: Chain): string {
+    return chainLogoMap[chain];
 }
