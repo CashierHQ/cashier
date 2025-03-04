f22b3364cef7f0cbe8a5e8d5fda9289ac9ac6ca5
cryptokovalyshyn <serhiikovalyshyn1@gmail.com>
2025-03-04 12:04:48 +0200

    FE - UI - Wallet Page (#198)
    * wallet page initial layout
    
    * finished wallet page layout
    
    * added mock button actions
    
    * added localization
    
    * added wallet tokens page ui
    
    * added mock nfts tab
    
    * added manage tokens ui
    
    * added ui for import token form
    
    * added token import review screen
    
    * created transaction history items
    
    * commit
    
    * added token details hero component
    
    * commit
    
    * fixed file naming
    
    * moved components to a different folder
    
    * fixed build errors
    
    * fixed naming for token manager
    
    * fixed naming for import token
    
    * fixed naming for token details
    
    * updated localization
    
    * updated transaction list component
    
    * extracted transaction history into a separate ui component
    
    * fixed scroll for token details
    
    * added temporary banner for nft
    
    * added props types for token details
    
    * fixed localization, added props to review
    
    * fixed index canister id validation
    
    * added basic search to manage tokens page
    
    * updated main wallet page
    
    * added hyphens, when no usd conversion for token card
    
    * refactored mock data, created a styled link component
    
    * changed wallet button to navigate to wallet page
    
    * added unit test for helpers and transactionHistoryService
    
    * fixed index canister id validation

---
+++ src/cashier_frontend/src/pages/wallet/layout.tsx
@@ -0,0 +1,15 @@
+import { WalletHeader } from "@/components/wallet/header";
+import { Outlet, useNavigate } from "react-router-dom";
+
+export default function WalletLayout() {
+    const navigate = useNavigate();
+
+    const closeWallet = () => navigate("/");
+
+    return (
+        <div className="flex flex-col h-dvh">
+            <WalletHeader onClose={closeWallet} />
+            <Outlet />
+        </div>
+    );
+}
