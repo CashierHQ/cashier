5a12c8ddf161c2c8423920bed35ac7a729f0d511
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-03-30 16:21:04 +0700

    Fe/send token functional (#241)
    * Add send asset functionality and transaction status management
    
    * Refactor asset transfer logic and integrate TokenUtilService for transaction handling
    
    * Add ExternalLink component and integrate it into SendTransactionStatus for transaction viewing
    
    * Add ExternalLink component and integrate it into SendTransactionStatus for transaction viewing

--- src/cashier_frontend/src/services/tokenUtils.service.ts
+++ src/cashier_frontend/src/services/tokenUtils.service.ts
@@ -1,8 +1,15 @@
+import { Agent, HttpAgent, Identity } from "@dfinity/agent";
+import { PartialIdentity } from "@dfinity/identity";
 import { IcrcLedgerCanister, IcrcTokenMetadata, mapTokenMetadata } from "@dfinity/ledger-icrc";
 import { Principal } from "@dfinity/principal";
 import { defaultAgent, Token, TokenAmountV2 } from "@dfinity/utils";
 
-export abstract class TokenUtilService {
+export class TokenUtilService {
+    private agent: Agent;
+
+    constructor(identity?: Identity | PartialIdentity | undefined) {
+        this.agent = HttpAgent.createSync({ identity, host: "https://icp0.io" });
+    }
     public static async getTokenMetadata(tokenAddres: string) {
         const { metadata } = IcrcLedgerCanister.create({
             agent: defaultAgent(),
@@ -36,5 +43,30 @@ export abstract class TokenUtilService {
         const tokenV2 = TokenAmountV2.fromUlps({ amount, token: tokenMetadata as Token });
         const upls = tokenV2.toUlps();
         return Number(upls) / 10 ** tokenV2.token.decimals;
+    }
+
+    // the amount is in human readable format
+    async transferTo(receiverAddress: string, tokenAddress: string, amount: number) {
+        const ledgerCanister = IcrcLedgerCanister.create({
+            agent: this.agent,
+            canisterId: Principal.fromText(tokenAddress),
+        });
+
+        const tokenMetadata = await TokenUtilService.getTokenMetadata(tokenAddress);
+        const tokenV2 = TokenAmountV2.fromNumber({ amount, token: tokenMetadata as Token });
+
+        const transfer_amount = tokenV2.toE8s();
+
+        return await ledgerCanister.transfer({
+            to: {
+                owner: Principal.fromText(receiverAddress),
+                subaccount: [],
+            },
+            amount: transfer_amount,
+            fee: undefined,
+            memo: undefined,
+            from_subaccount: undefined,
+            created_at_time: undefined,
+        });
     }
 }
