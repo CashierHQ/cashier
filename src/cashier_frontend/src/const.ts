9a62354485ffc4a0e67ca93f5255d1546b715eed
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-04-07 14:34:09 +0700

    Update IC_HOST constant to use the correct ICP host URL

--- src/cashier_frontend/src/const.ts
+++ src/cashier_frontend/src/const.ts
@@ -1,4 +1,4 @@
 export const BACKEND_CANISTER_ID = import.meta.env.VITE_BACKEND_CANISTER_ID;
 export const TOKEN_STORAGE_CANISTER_ID = import.meta.env.VITE_TOKEN_STORAGE_CANISTER_ID;
 export const IC_EXPLORER_BASE_URL = import.meta.env.VITE_IC_EXPLORER_BASE_URL;
-export const IC_HOST = "https://ic0.app";
+export const IC_HOST = "https://icp0.io";
