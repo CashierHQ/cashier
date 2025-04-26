85bab71c0552e242fd026067f451e9b200928f39
Sebastian Rubina <sebastian.rubina@icloud.com>
2025-04-25 23:00:59 -0400

    Sidebar

--- src/cashier_frontend/src/constants/otherConst.ts
+++ src/cashier_frontend/src/constants/otherConst.ts
@@ -4,6 +4,7 @@ import { LuCompass } from "react-icons/lu";
 import { CiSettings } from "react-icons/ci";
 import { LuGanttChartSquare } from "react-icons/lu";
 import React from "react";
+import { CircleHelp, Link } from "lucide-react";
 
 export const INCREASE = "increase";
 export const DECREASE = "decrease";
@@ -11,25 +12,41 @@ export const DECREASE = "decrease";
 export const BOTTOM_MENU_ITEMS: SidebarMenuItem[] = [
     {
         title: "FAQ",
-        icon: React.createElement(CiSettings, { size: 22 }),
-        docSource: "https://doc.clickup.com/9012452868/d/h/8cjy7g4-4812/2b32fa3ed781459",
+        icon: React.createElement(CircleHelp, { size: 22 }),
+        onClick: () =>
+            window.open(
+                "https://doc.clickup.com/9012452868/d/h/8cjy7g4-4812/2b32fa3ed781459",
+                "_blank",
+            ),
     },
 ];
 
 export const TOP_MENU_ITEMS: SidebarMenuItem[] = [
     {
         title: "About us",
-        icon: React.createElement(FaLink, { size: 22 }),
-        docSource: "https://doc.clickup.com/9012452868/d/h/8cjy7g4-4792/cd2e9646a3a8a0a",
+        icon: React.createElement(Link, { size: 22 }),
+        onClick: () =>
+            window.open(
+                "https://doc.clickup.com/9012452868/d/h/8cjy7g4-4792/cd2e9646a3a8a0a",
+                "_blank",
+            ),
     },
     {
         title: "Explore Cashier",
         icon: React.createElement(LuCompass, { size: 22 }),
-        docSource: "https://doc.clickup.com/9012452868/d/h/8cjy7g4-4852/8a86f74b2d7ea73",
+        onClick: () =>
+            window.open(
+                "https://doc.clickup.com/9012452868/d/h/8cjy7g4-4852/8a86f74b2d7ea73",
+                "_blank",
+            ),
     },
     {
         title: "Cashier project overview",
         icon: React.createElement(LuGanttChartSquare, { size: 22 }),
-        docSource: "https://doc.clickup.com/9012452868/d/h/8cjy7g4-4292/9a3796b6e853ef0",
+        onClick: () =>
+            window.open(
+                "https://doc.clickup.com/9012452868/d/h/8cjy7g4-4292/9a3796b6e853ef0",
+                "_blank",
+            ),
     },
 ];
