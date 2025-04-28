e89aa87b52a4fade6e0b63fd1bc7fa8783abacfd
Sebastian Rubina <sebastian.rubina@icloud.com>
2025-04-27 23:34:51 -0400

    fix

--- src/cashier_frontend/src/components/claim-page/token-item.tsx
+++ src/cashier_frontend/src/components/claim-page/token-item.tsx
@@ -5,37 +5,22 @@ import { TokenUtilService } from "@/services/tokenUtils.service";
 import { TokenMetadata } from "@/types/fungible-token.speculative";
 import { IcrcTokenMetadata } from "@dfinity/ledger-icrc";
 import { Loader2 } from "lucide-react";
-
+import { useTokenStore } from "@/stores/tokenStore";
 interface TokenItemProps {
     asset: LinkDetailModel["asset_info"][0];
 }
 
 const TokenItem: React.FC<TokenItemProps> = ({ asset }) => {
-    const tokenLogo = getTokenImage(asset.address);
-    const [metadata, setMetadata] = useState<IcrcTokenMetadata | null>(null);
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        const getTokenMetadata = async () => {
-            const tokenMetadata = await TokenUtilService.getTokenMetadata(asset.address);
-            return tokenMetadata;
-        };
-        getTokenMetadata().then((res) => {
-            if (res) {
-                console.log(`Res for ${asset.address}`, res);
-                setMetadata(res);
-            }
-            setIsLoading(false);
-        });
-    }, []);
+    const { getToken } = useTokenStore();
+    const token = getToken(asset.address);
 
     return (
         <div className="flex justify-between ml-1">
             <div className="flex items-center">
                 <div className="flex gap-x-5 items-center">
                     <img
-                        src={tokenLogo}
-                        alt={metadata?.symbol}
+                        src={token?.logo}
+                        alt={token?.symbol}
                         className="w-10 h-10 rounded-sm mr-3"
                         onError={(e) => {
                             e.currentTarget.onerror = null;
@@ -43,13 +28,7 @@ const TokenItem: React.FC<TokenItemProps> = ({ asset }) => {
                         }}
                     />
                 </div>
-                {isLoading ? (
-                    <div className="mr-3">
-                        <Loader2 className="animate-spin rounded-full h-4 w-4" color="green" />
-                    </div>
-                ) : (
-                    <div className="mr-3">{metadata?.symbol}</div>
-                )}
+                <div className="mr-3">{token?.symbol || asset.address}</div>
             </div>
             <div className="text-green">{Number(asset.amountPerUse) / 10 ** 8}</div>
         </div>
