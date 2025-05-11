8dacc00b0640ed3518b2b4b9dddeb7e1d23b4291
Sebastian Rubina <sebastian.rubina@icloud.com>
2025-05-11 00:48:50 -0400

    step 1 link claim UI improvements

--- src/cashier_frontend/src/components/link-card-without-phone-frame.tsx
+++ src/cashier_frontend/src/components/link-card-without-phone-frame.tsx
@@ -2,57 +2,40 @@ import { cn } from "@/lib/utils";
 import { useEffect, useState } from "react";
 
 export default function LinkCardWithoutPhoneFrame({
-    src,
+    displayComponent,
     title,
     message,
     label,
     onClaim,
     disabled,
     logoFallback,
+    showHeader = false,
+    headerText = "",
 }: {
-    src?: string;
-    header?: string;
+    displayComponent?: React.ReactNode;
     title: string;
     message: string;
     label: string;
     onClaim?: () => void;
     disabled?: boolean;
     logoFallback?: string;
+    showHeader?: boolean;
+    headerText?: string;
 }) {
-    // Initial src with fallback
-    const [imgSrc, setImgSrc] = useState(src || "/defaultLinkImage.png");
-
-    // Handle image load error
-    const handleImageError = () => {
-        // If the current src is already the default, try another fallback
-        if (logoFallback) {
-            setImgSrc(logoFallback);
-        }
-        if (imgSrc === "/defaultLinkImage.png") {
-            setImgSrc("/token-basket-default.png");
-        } else {
-            setImgSrc("/defaultLinkImage.png");
-        }
-    };
-
-    useEffect(() => {
-        if (logoFallback) {
-            setImgSrc(logoFallback);
-        }
-    }, [logoFallback]);
-
     return (
-        <div className="w-full flex flex-col items-center bg-lightgreen rounded-xl mt-5 py-5 px-8">
-            <div className="w-full flex flex-col items-center bg-lightgreen rounded-xl mt-3 p-3">
-                <img
-                    src={imgSrc}
-                    alt="Link template"
-                    className="w-[200px]"
-                    onError={handleImageError}
-                />
+        <div className="w-full flex relative flex-col items-center bg-lightgreen rounded-xl py-5 px-8">
+            {showHeader && (
+                <div className="w-full absolute top-0 left-0 flex flex-col items-center bg-green rounded-t-xl">
+                    <h3 className="text-[18px] text-white font-medium py-1">{headerText}</h3>
+                </div>
+            )}
+            <div
+                className={`w-full flex flex-col items-center bg-lightgreen rounded-xl p-3 ${showHeader ? "mt-5" : "mt-3"}`}
+            >
+                {displayComponent}
                 <div className="mb-8 text-center">
-                    <h3 className="text-lg font-semibold py-2">{title}</h3>
-                    <h3 className="text-md text-[#475467]">{message}</h3>
+                    <h3 className="text-[16px] font-medium mt-2">{title}</h3>
+                    <h3 className="text-[14px] font-light text-[#475467]/80 mt-1">{message}</h3>
                 </div>
 
                 {disabled ? (
@@ -67,7 +50,7 @@ export default function LinkCardWithoutPhoneFrame({
                 ) : (
                     <div
                         className={cn(
-                            "text-white bg-green rounded-full py-3 px-8 mt-3 text-base w-[100%] text-center",
+                            "text-white bg-green rounded-full py-2 px-8 mt-3 text-base w-[100%] text-center",
                             onClaim ? "cursor-pointer" : "",
                         )}
                         onClick={onClaim}
--- src/cashier_frontend/src/pages/[id]/LinkCardPage.tsx
+++ src/cashier_frontend/src/pages/[id]/LinkCardPage.tsx
@@ -1,6 +1,8 @@
 import LinkCardWithoutPhoneFrame from "@/components/link-card-without-phone-frame";
 import { useTokens } from "@/hooks/useTokens";
+import { LINK_TYPE } from "@/services/types/enum";
 import { LinkDetailModel } from "@/services/types/link.service.types";
+import { convertTokenAmountToNumber } from "@/utils";
 import { FC } from "react";
 
 type LinkCardPageProps = {
@@ -9,7 +11,7 @@ type LinkCardPageProps = {
 };
 
 export const LinkCardPage: FC<LinkCardPageProps> = ({ linkData, onClickClaim }) => {
-    const { getToken } = useTokens();
+    const { getToken, rawTokenList } = useTokens();
 
     // Get token data and image
     const tokenAddress = linkData?.asset_info?.[0]?.address;
@@ -22,15 +24,126 @@ export const LinkCardPage: FC<LinkCardPageProps> = ({ linkData, onClickClaim }) 
     console.log("LinkCardPage tokenLogo: ", tokenLogo);
     console.log("LinkCardPage logoFallback: ", logoFallback);
 
+    console.log("LinkCardPage linkData: ", linkData);
+
+    const getTitleForLink = () => {
+        switch (linkData?.linkType) {
+            case LINK_TYPE.SEND_TIP:
+            case LINK_TYPE.SEND_AIRDROP:
+            case LINK_TYPE.RECEIVE_PAYMENT:
+                const token = rawTokenList.find(
+                    (token) => token.address === linkData?.asset_info?.[0]?.address,
+                );
+                const amount =
+                    Number(linkData?.asset_info?.[0]?.amountPerUse) / 10 ** (token?.decimals ?? 0);
+                return `${amount} ${token?.symbol}`;
+            case LINK_TYPE.SEND_AIRDROP:
+                return "Send Airdrop";
+            case LINK_TYPE.SEND_TOKEN_BASKET:
+                return "";
+            default:
+                return "";
+        }
+    };
+
+    const getMessageForLink = () => {
+        switch (linkData?.linkType) {
+            case LINK_TYPE.SEND_TIP:
+                return "Congratulations, you received a tip!";
+            case LINK_TYPE.SEND_AIRDROP:
+                return "Congratulations, you received an airdrop!";
+            case LINK_TYPE.SEND_TOKEN_BASKET:
+                return "Congratulations, you received a token basket!";
+            case LINK_TYPE.RECEIVE_PAYMENT:
+                const token = rawTokenList.find(
+                    (token) => token.address === linkData?.asset_info?.[0]?.address,
+                );
+                const amount =
+                    Number(linkData?.asset_info?.[0]?.amountPerUse) / 10 ** (token?.decimals ?? 0);
+                return `You have been requested to pay the amount of ${amount} ${token?.symbol}`;
+            default:
+                return "";
+        }
+    };
+
+    const getDisplayComponentForLink = () => {
+        switch (linkData?.linkType) {
+            case LINK_TYPE.SEND_TIP:
+            case LINK_TYPE.SEND_AIRDROP:
+            case LINK_TYPE.RECEIVE_PAYMENT:
+                const token = rawTokenList.find(
+                    (token) => token.address === linkData?.asset_info?.[0]?.address,
+                );
+                console.log("token logo: ", token?.logo);
+                return (
+                    <img
+                        src={token?.logo || logoFallback}
+                        alt="Tip Link"
+                        className="w-[200px] rounded-3xl"
+                        onError={(e) => {
+                            console.log("error: ", e);
+                            e.currentTarget.src = logoFallback;
+                        }}
+                    />
+                );
+            case LINK_TYPE.SEND_TOKEN_BASKET:
+                const tokens = linkData?.asset_info?.map((asset) => {
+                    const token = rawTokenList.find((token) => token.address === asset.address);
+                    return {
+                        symbol: token?.symbol,
+                        amount: asset.amountPerUse,
+                        logo: token?.logo,
+                        decimals: token?.decimals,
+                    };
+                });
+                return (
+                    <div className="w-[200px] min-h-[200px] bg-white rounded-2xl p-4 flex flex-col justify-center gap-4">
+                        {tokens?.map((token) => (
+                            <div className="flex items-center">
+                                <img
+                                    src={token.logo}
+                                    alt={token.symbol}
+                                    className="w-8 h-8 rounded-sm"
+                                    onError={(e) => {
+                                        e.currentTarget.src = logoFallback;
+                                    }}
+                                />
+                                <p className="text-[20px] font-light ml-2">
+                                    {Number(token.amount) / 10 ** (token?.decimals ?? 0)}{" "}
+                                    {token.symbol || "Token"}
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                );
+            default:
+                return "";
+        }
+    };
+
+    const getHeaderTextForLink = () => {
+        switch (linkData?.linkType) {
+            case LINK_TYPE.SEND_AIRDROP:
+                const token = rawTokenList.find(
+                    (token) => token.address === linkData?.asset_info?.[0]?.address,
+                );
+                return `${linkData.useActionCounter}/${linkData.maxActionNumber} claimed`;
+            default:
+                return "";
+        }
+    };
+
     return (
         <LinkCardWithoutPhoneFrame
             label="Claim"
-            src={tokenLogo}
-            message={linkData?.description ?? ""}
-            title={linkData?.title ?? ""}
+            displayComponent={getDisplayComponentForLink()}
+            message={getMessageForLink()}
+            title={getTitleForLink()}
             onClaim={onClickClaim}
             disabled={linkData === undefined}
             logoFallback={logoFallback}
+            showHeader={linkData?.linkType === LINK_TYPE.SEND_AIRDROP}
+            headerText={getHeaderTextForLink()}
         />
     );
 };
