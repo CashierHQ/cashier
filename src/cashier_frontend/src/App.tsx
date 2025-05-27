dfd0d9084616799338ed8a351e348ae38649e2fe
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-05-27 17:13:24 +0700

    etsting 5 minutes timeout

--- src/cashier_frontend/src/App.tsx
+++ src/cashier_frontend/src/App.tsx
@@ -50,7 +50,7 @@ function App() {
             signers={signers}
             signerClientOptions={{
                 targets,
-                maxTimeToLive: 3_600_000_000_000n,
+                maxTimeToLive: 300_000_000_000n,
             }}
             discoverExtensionSigners={true}
         >
