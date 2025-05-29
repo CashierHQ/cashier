a8b00263c1f330acee3e59b4317cf209fafcb33e
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-05-29 17:44:42 +0700

    Update cache control max-age to 10 minutes and remove unnecessary blank line in DetailPage component

--- src/cashier_frontend/src/pages/details/[id]/index.tsx
+++ src/cashier_frontend/src/pages/details/[id]/index.tsx
@@ -43,7 +43,6 @@ import { formatNumber } from "@/utils/helpers/currency";
 import { Share2 } from "lucide-react";
 import { toast } from "sonner";
 
-
 export default function DetailPage() {
     const { linkId } = useParams();
     const identity = useIdentity();
