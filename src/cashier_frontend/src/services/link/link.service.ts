94efbd817f55683d4dca0fdac282dd9223164b8a
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-06-11 02:14:18 +0700

    clean up

--- src/cashier_frontend/src/services/link/link.service.ts
+++ src/cashier_frontend/src/services/link/link.service.ts
@@ -104,7 +104,6 @@ class LinkService {
             agent,
             canisterId: BACKEND_CANISTER_ID,
         });
-        console.log("LinkService initialized with actor:", this.actor);
     }
 
     async getLinkList() {
