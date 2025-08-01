0485bb4130e08f4efae0a95e90a948b7e8d9e62e
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-08-02 01:11:18 +0700

    Remove unused types and modules from the cashier_backend codebase

--- src/cashier_backend/src/types/common.rs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2025 Cashier Protocol Labs
-// Licensed under the MIT License (see LICENSE file in the project root)
-
-use super::error::CanisterError;
-
-// Add this to the error.rs file
-pub type CanisterResult<T> = Result<T, CanisterError>;
