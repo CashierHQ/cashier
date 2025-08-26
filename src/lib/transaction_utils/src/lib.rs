bbb6d0f4753c10abc2c1199f4b31f52c10f8ac7d
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-08-26 16:56:58 +0700

    add comment

--- src/lib/transaction_utils/src/lib.rs
+++ src/lib/transaction_utils/src/lib.rs
@@ -5,6 +5,8 @@ use crate::types::CanisterCall;
 
 pub mod types;
 
+// build a canister call payload with base64 encoded argument
+// this canister call only execute on client side
 pub fn build_canister_call<T: CandidType>(
     canister_id: &Principal,
     method: impl Into<String>,
