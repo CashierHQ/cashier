ceab8289b59104757234134c7f7df867e4033051
barchitect <tbng84@gmail.com>
2025-09-08 10:45:02 +0700

    init permissions integration tests

--- src/integration_tests/tests/gate_service_backend/init_and_upgrade.rs
+++ src/integration_tests/tests/gate_service_backend/init_and_upgrade.rs
@@ -3,16 +3,20 @@ use gate_service_types::auth::Permission;
 use crate::utils::{principal::TestUser, with_pocket_ic_context};
 
 #[tokio::test]
-async fn should_allow_admin_to_get_permissions() {
+async fn should_init_with_gate_creator_permissions_for_cashier_backend() {
     with_pocket_ic_context::<_, ()>(async move |ctx| {
         // Arrange
         let admin = TestUser::GateServiceBackendAdmin.get_principal();
         let admin_client = ctx.new_gate_service_backend_client(admin);
 
-        let permissions = admin_client.admin_permissions_get(admin).await.unwrap();
+        // Act
+        let permissions = admin_client
+            .admin_permissions_get(ctx.cashier_backend_principal)
+            .await
+            .unwrap();
 
         // Assert
-        assert_eq!(vec![Permission::Admin], permissions);
+        assert_eq!(vec![Permission::GateCreator], permissions);
 
         Ok(())
     })
