6a2aef404c9770f9505387deef4380fe3c6433d6
Francesco <ufoscout@gmail.com>
2025-09-16 16:09:44 +0200

    Add IcpSwapClient

--- src/cashier_frontend/src/services/token_price/index.ts
+++ src/cashier_frontend/src/services/token_price/index.ts
@@ -1,3 +1,12 @@
-export * as icsPool from "./icswap/icpswapPool";
-export * as icsIndexNode from "./icswap/icpswapNodeIndex";
+import { Result } from "ts-results";
+
+export * as icpSwapPool from "./icpswap/icpswapPool";
+export * as icpSwapIndexNode from "./icpswap/icpswapNodeIndex";
 export * as kongBackend from "./kongswap/kongBackend";
+
+/// Interface for token price clients
+export interface TokenPriceClient {
+    
+    //// @returns An array of pairs (token_address, token_price)
+    getTokenPrices(): Promise<Result<Record<string, number>, Error>>;
+}
