a3e44dd2ee5749714c2f8024733ea91ca218680c
Francesco <ufoscout@gmail.com>
2025-09-23 16:36:29 +0200

    Add demo comments

--- src/cashier_frontend_new/src/routes/+page.svelte
+++ src/cashier_frontend_new/src/routes/+page.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
+  // DEMO: `resolve` is a helper function that statically checks link validity at compile time
   import { resolve } from '$app/paths';
+  // DEMO: `m` is a helper function that statically checks message validity at compile time
 	import { m } from '$lib/paraglide/messages.js';
 </script>
 
+<!-- DEMO: no need for a dedicated tailwind configuration because this uses version 4 by default -->
 <div class="hero bg-base-200 min-h-screen">
   <div class="hero-content text-center">
     <div class="max-w-md">
+
+      <!-- DEMO: a message is statically checked and, if possible, resolved at compile time -->
       <h1 class="text-5xl font-bold">{m.welcome()}</h1>
-      <p class="py-6">Visit <a class="link" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-      <p class="py-6">Visit blog: <a class="link" href={resolve('/blog/blog_title')}>BLOG!!</a></p>
+
+      <!-- DEMO: a link to a page, the linked is statically checked at compile time. So all links are always correct -->
+      <p class="py-6">Blog: <a class="link" href={resolve('/blog/blog_title')}>Blog</a></p>
+      
       <p class="py-6"><a class="link" href={resolve('/about')}>About</a></p>
     </div>
   </div>
---
+++ src/cashier_frontend_new/src/routes/props/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts" >
+    interface Props {
+		name: string;
+		version: number;
+        description: string;
+	}
+
+	let { name, version, description }: Props = $props();
+
+</script>
+
+<p>
+	Project name <code>{name}</code>, version <code>{version}</code>is {description}.
+</p>
