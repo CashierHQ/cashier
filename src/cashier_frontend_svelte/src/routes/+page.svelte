8387b5d3d9e8ec3327219b0215af8770ea2d62c5
Francesco <ufoscout@gmail.com>
2025-09-23 14:55:50 +0200

    Add svelte stub for new frontend

---
+++ src/cashier_frontend_svelte/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { m } from '$lib/paraglide/messages.js';
+</script>
+
+<div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">{m.welcome()}</h1>
+      <p class="py-6">Visit <a class="link" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+      <p class="py-6">Visit blog: <a class="link" href="/blog/blog_title">BLOG!!</a></p>
+      <p class="py-6"><a class="link" href="/about">About</a></p>
+    </div>
+  </div>
+</div>
---
+++ src/cashier_frontend_svelte/src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/state';
+    import { m } from '$lib/paraglide/messages.js';
+
+    let slug = page.params.slug || 'empty';
+
+</script>
+
+<h1>{m.blog_title({ title: slug })}</h1>
+
+<p class="py-6"><a class="link" href="/blog/{slug}1">another blog</a></p>
+<p class="py-6"><a class="link" href="/">home</a></p>
