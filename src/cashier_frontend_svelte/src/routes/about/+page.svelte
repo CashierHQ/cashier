f01e9e5428afcab0b9425bae01abe5f00c61dccd
Francesco <ufoscout@gmail.com>
2025-09-23 14:55:50 +0200

    Add svelte stub for new frontend

---
+++ src/cashier_frontend_svelte/src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">ABOUT MANY THINGS</h1>
+      <p class="py-6"><a class="link" href="/">home</a></p>
+    </div>
+  </div>
+</div>
