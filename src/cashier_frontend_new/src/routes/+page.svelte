d0b40860ecf4e4ddbd8dbb1dbad1455504b75b73
Francesco <ufoscout@gmail.com>
2025-09-23 15:54:27 +0200

    fix lint configuration

--- src/cashier_frontend_new/src/routes/+page.svelte
+++ src/cashier_frontend_new/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { resolve } from '$app/paths';
 	import { m } from '$lib/paraglide/messages.js';
 </script>
 
@@ -7,8 +8,8 @@
     <div class="max-w-md">
       <h1 class="text-5xl font-bold">{m.welcome()}</h1>
       <p class="py-6">Visit <a class="link" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-      <p class="py-6">Visit blog: <a class="link" href="/blog/blog_title">BLOG!!</a></p>
-      <p class="py-6"><a class="link" href="/about">About</a></p>
+      <p class="py-6">Visit blog: <a class="link" href={resolve('/blog/blog_title')}>BLOG!!</a></p>
+      <p class="py-6"><a class="link" href={resolve('/about')}>About</a></p>
     </div>
   </div>
 </div>
--- src/cashier_frontend_new/src/routes/blog/[slug]/+page.svelte
+++ src/cashier_frontend_new/src/routes/blog/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/state';
     import { m } from '$lib/paraglide/messages.js';
+    import { resolve } from '$app/paths';
 
     let slug = page.params.slug || 'empty';
 
@@ -8,5 +9,7 @@
 
 <h1>{m.blog_title({ title: slug })}</h1>
 
-<p class="py-6"><a class="link" href="/blog/{slug}1">another blog</a></p>
-<p class="py-6"><a class="link" href="/">home</a></p>
+<p class="py-6"><a class="link" href={resolve(`/blog/[slug]`, {
+	slug: slug + "_1"
+})}>another blog</a></p>
+<p class="py-6"><a class="link" href={resolve("/")}>home</a></p>
