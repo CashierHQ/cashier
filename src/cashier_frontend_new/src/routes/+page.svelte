93b2270da8ab1d94d33eaa804244463ac852c5d8
Francesco <ufoscout@gmail.com>
2025-09-24 16:24:23 +0200

    Add env files

--- src/cashier_frontend_new/src/routes/+page.svelte
+++ src/cashier_frontend_new/src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   // DEMO: `resolve` is a helper function that statically checks link validity at compile time
   import { resolve } from '$app/paths';
+  import { THE_ANSWER } from '$lib/constants';
   // DEMO: `m` is a helper function that statically checks message validity at compile time
 	import { m } from '$lib/paraglide/messages.js';
+
+  console.log("The answer is: " + THE_ANSWER);
 </script>
 
 <!-- DEMO: no need for a dedicated tailwind configuration because this uses version 4 by default -->
