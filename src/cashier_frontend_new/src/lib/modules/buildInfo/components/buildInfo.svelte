5ede4edd863ed185432f97ddff6210b6ee7fe466
Francesco <ufoscout@gmail.com>
2025-09-25 16:04:05 +0200

    Add dev page

---
+++ src/cashier_frontend_new/src/lib/modules/buildInfo/components/buildInfo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import * as buildData from "../constants";
+</script>
+
+<code>
+  Build info<br/>
+  ----------<br/>
+  App version: <strong>{buildData.BUILD_APP_VERSION}</strong><br/>
+  Timestamp: <strong>{buildData.BUILD_TIMESTAMP}</strong><br/>
+  Commit hash: <strong>{buildData.BUILD_COMMIT_HASH}</strong><br/>
+</code>
---
+++ src/cashier_frontend_new/src/routes/dev/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import BuildInfo from "$lib/modules/buildInfo/components/buildInfo.svelte";
+</script>
+
+<BuildInfo />
