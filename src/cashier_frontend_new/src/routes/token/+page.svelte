01ce0681e652811749da35d497fdc4c931c497e9
Francesco <ufoscout@gmail.com>
2025-09-26 17:28:34 +0200

    Add code stub for token metadata

--- src/cashier_frontend_new/src/routes/token/+page.svelte
+++ src/cashier_frontend_new/src/routes/token/+page.svelte
@@ -1,33 +1,45 @@
 <script lang="ts">
-  import { tokenPriceService } from "$modules/token/services/tokenPrice.svelte";
+    import TokenDetail from "$modules/token/components/tokenDetail.svelte";
+    import { tokenMetadataService } from "$modules/token/services/tokenMetadata";
+  import { tokenPriceService } from "$modules/token/services/tokenPrice";
   import type { TokenPrice } from "$modules/token/types";
+    import type { IcrcTokenMetadata } from "@dfinity/ledger-icrc";
   import { createQuery } from "@tanstack/svelte-query";
 
   // Create a query to fetch token prices using the TokenPriceService
-  const tokenQuery = createQuery<TokenPrice[]>({
+  const tokenPriceQuery = createQuery<TokenPrice[]>({
     queryKey: ["tokenPrices"],
     queryFn: async () => {
       return tokenPriceService.getTokens();
     },
   });
+
+  // Create a query to fetch token metadata
+  const tokenMetadataQuery = (tokenAddress: string) => createQuery<IcrcTokenMetadata | undefined>({
+    queryKey: ["tokenMetadata", tokenAddress],
+    queryFn: async () => {
+      return tokenMetadataService.getTokenMetadata(tokenAddress);
+    },
+  })
+
+  const tokenMetadata = tokenMetadataQuery("tokenAddress");
+  $tokenMetadata.isPending;
 </script>
 
 <div>
-  {#if $tokenQuery.isPending}
+  {#if $tokenPriceQuery.isPending}
     Loading...
   {/if}
-  {#if $tokenQuery.error}
+  {#if $tokenPriceQuery.error}
     An error has occurred:
-    {$tokenQuery.error.message}
+    {$tokenPriceQuery.error.message}
   {/if}
-  {#if $tokenQuery.isSuccess}
+  {#if $tokenPriceQuery.isSuccess}
     <div>
       <h2>Token Prices</h2>
       <ul>
-        {#each $tokenQuery.data as token (token.address)}
-          <li>
-            {token.symbol} ({token.standard}): ${token.priceUSD.toFixed(5)}
-          </li>
+        {#each $tokenPriceQuery.data.slice(0, 10) as token (token.address)}
+          <TokenDetail price={token} />
         {/each}
       </ul>
     </div>
