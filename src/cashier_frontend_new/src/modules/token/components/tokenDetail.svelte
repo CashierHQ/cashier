8197f00f2d5ce667eee0f0ac29e36bd340690c37
Francesco <ufoscout@gmail.com>
2025-09-29 09:17:01 +0200

    remove react query

--- src/cashier_frontend_new/src/modules/token/components/tokenDetail.svelte
+++ src/cashier_frontend_new/src/modules/token/components/tokenDetail.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import { managedState } from "$lib/managedState/managedState.svelte";
   import { tokenMetadataService } from "$modules/token/services/tokenMetadata";
   import type { TokenPrice } from "$modules/token/types";
   import type { IcrcTokenMetadata } from "@dfinity/ledger-icrc";
-  import { createQuery } from "@tanstack/svelte-query";
+
+  // DEMO: how to use managedState recursively
 
   interface Props {
     price: TokenPrice;
@@ -10,28 +12,28 @@
 
   let data: Props = $props();
 
-  // Create a query to fetch token metadata
-  const tokenMetadataQuery = createQuery<IcrcTokenMetadata | undefined>({
-    queryKey: ["tokenMetadata", data.price.address],
+  let tokenMetadataQuery = managedState<IcrcTokenMetadata | undefined>({
     queryFn: async () => {
       return tokenMetadataService.getTokenMetadata(data.price.address);
     },
+    // staleTime: 5_000,
+    // refetchInterval: 10_000,
+    // persistedKey: ["tokenMetadata", data.price.address],
   });
 </script>
 
 <li>
   {data.price.symbol} ({data.price.standard}): ${data.price.priceUSD.toFixed(5)}
-  - metadata:
-
-  {#if $tokenMetadataQuery.isPending}
+  - metadata: -- QUERY 1:
+  {#if tokenMetadataQuery.isLoading}
     Loading...
   {/if}
-  {#if $tokenMetadataQuery.error}
+  {#if tokenMetadataQuery.error}
     Cannot fetch token metadata
     <!-- {$tokenMetadataQuery.error.message} -->
   {/if}
-  {#if $tokenMetadataQuery.isSuccess}
-    decimals: {$tokenMetadataQuery.data?.decimals} - fee: {$tokenMetadataQuery
+  {#if tokenMetadataQuery.isSuccess}
+    decimals: {tokenMetadataQuery.data?.decimals} - fee: {tokenMetadataQuery
       .data?.fee}
   {/if}
 </li>
