72c6063f9b56920911f0a9ff66d7baf13a6444ea
Francesco <ufoscout@gmail.com>
2025-09-29 09:17:01 +0200

    remove react query

--- src/cashier_frontend_new/src/routes/token/+page.svelte
+++ src/cashier_frontend_new/src/routes/token/+page.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
   import { resolve } from "$app/paths";
-
-  // import TokenDetail from "$modules/token/components/tokenDetail.svelte";
-  import TokenDetail from "$modules/token/components/tokenDetail_new.svelte";
+  import { managedState } from "$lib/managedState/managedState.svelte";
+  import TokenDetail from "$modules/token/components/tokenDetail.svelte";
   import { tokenPriceService } from "$modules/token/services/tokenPrice";
   import type { TokenPrice } from "$modules/token/types";
-  import { createQuery } from "@tanstack/svelte-query";
 
   // Create a query to fetch token prices using the TokenPriceService
-  const tokenPriceQuery = createQuery<TokenPrice[]>({
-    queryKey: ["tokenPrices"],
+  const tokenPriceQuery = managedState<TokenPrice[]>({
+    persistedKey: ["tokenPrices"],
     queryFn: async () => {
       console.log("fetching token prices from react-query");
       return tokenPriceService.getTokens();
@@ -21,18 +19,18 @@
 <p class="py-6"><a class="link" href={resolve("/")}>Go to Home</a></p>
 
 <div>
-  {#if $tokenPriceQuery.isPending}
+  {#if tokenPriceQuery.isLoading}
     Loading...
   {/if}
-  {#if $tokenPriceQuery.error}
+  {#if tokenPriceQuery.error}
     An error has occurred:
-    {$tokenPriceQuery.error.message}
+    {tokenPriceQuery.error}
   {/if}
-  {#if $tokenPriceQuery.isSuccess}
+  {#if tokenPriceQuery.isSuccess && tokenPriceQuery.data}
     <div>
       <h2>Token Prices</h2>
       <ul>
-        {#each $tokenPriceQuery.data.slice(0, 10) as token (token.address)}
+        {#each tokenPriceQuery.data.slice(0, 10) as token (token.address)}
           <TokenDetail price={token} />
         {/each}
       </ul>
