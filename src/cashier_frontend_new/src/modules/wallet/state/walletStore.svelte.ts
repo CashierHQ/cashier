18c75f45c8760515f4bad319a32b9e3888635a3e
barchitect <tbng84@gmail.com>
2025-09-30 10:36:41 +0700

    relocate files

--- src/cashier_frontend_new/src/modules/wallet/state/walletStore.svelte.ts
+++ src/cashier_frontend_new/src/modules/wallet/state/walletStore.svelte.ts
@@ -1,14 +1,11 @@
 import { managedState } from "$lib/managedState";
+import { tokenStorageService } from "$modules/token/services/tokenStorage";
 import type { TokenMetadata } from "$modules/token/types";
-import { tokenStorageService } from "../services/tokenStorage";
 
 export const listTokensQuery = managedState<TokenMetadata[]>({
   queryFn: async () => {
-    // Placeholder for actual token listing logic
     return tokenStorageService.listTokens();
   },
   persistedKey: ["listTokensQuery"],
   storageType: "localStorage",
 });
-
-export const walletStore = {};
