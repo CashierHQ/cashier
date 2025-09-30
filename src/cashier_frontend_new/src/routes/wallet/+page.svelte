0891bc51c23acb0024d78e7ed5eb5d125ebdf818
barchitect <tbng84@gmail.com>
2025-09-30 12:14:00 +0700

    refactor refresh query

--- src/cashier_frontend_new/src/routes/wallet/+page.svelte
+++ src/cashier_frontend_new/src/routes/wallet/+page.svelte
@@ -2,14 +2,11 @@
   import { resolve } from "$app/paths";
   import { accountState } from "$modules/shared/state/auth.svelte";
   import { listTokensQuery } from '$modules/wallet/state/walletStore.svelte';
-  
-  $effect(() => {
-    listTokensQuery.refresh();
-  });
 
   $effect(() => {
-    console.log("Account state changed:", accountState.account);
-    listTokensQuery.refresh();
+    if (accountState.account) {
+      listTokensQuery.refresh();
+    }
   });
 </script>
 
@@ -20,20 +17,19 @@
     Loading...
   {/if}
   {#if listTokensQuery.error}
-    An error has occurred:
-    {listTokensQuery.error}
+    <p style="color: red;">An error has occurred:
+    {listTokensQuery.error}</p>
   {/if}
   {#if listTokensQuery.isSuccess && listTokensQuery.data}
     <div>
-      <h2>Wallet Tokens</h2>
+      <h2>Wallet tokens</h2>
       <ul>
         {#each listTokensQuery.data as token (token.address)}
           <li>
             <strong>{token.symbol}</strong> - {token.name} <br />
             Address: {token.address} <br />
             Decimals: {token.decimals} <br />
-            Balance: {token.balance.toString()} <br />
-            Price USD: ${token.priceUSD.toFixed(5)} <br />
+            Balance: {token.balance.toString()} - Price: ${token.priceUSD.toFixed(5)} <br />
             Value USD: ${(token.balance * token.priceUSD).toFixed(5)} <br />
           </li>
           <hr />
