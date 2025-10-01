207cdd9273d5776def535a72d348172302c20695
Francesco <ufoscout@gmail.com>
2025-10-01 15:30:59 +0200

    state dependency tree - part 1

--- src/cashier_frontend_new/src/modules/links/services/cashierBackend.ts
+++ src/cashier_frontend_new/src/modules/links/services/cashierBackend.ts
@@ -2,6 +2,7 @@ import * as cashierBackend from "$lib/generated/cashier_backend/cashier_backend.
 import { responseToResult } from "$lib/result";
 import { authState } from "$modules/auth/state/auth.svelte";
 import { CASHIER_BACKEND_CANISTER_ID } from "$modules/shared/constants";
+import { accountState } from "$modules/shared/state/auth.svelte";
 import type { Result } from "ts-results-es";
 
 /**
@@ -14,7 +15,11 @@ class CanisterBackendService {
    * @throws Error if the user is not authenticated
    */
   #getActor(): cashierBackend._SERVICE {
-    if (authState.pnp && authState.pnp.isAuthenticated()) {
+    // console.log(
+    //       "Account state changed:", accountState.account
+    //       // "Links... for pnp: ", value
+    //     )
+    if (accountState.account && authState.pnp && authState.pnp.isAuthenticated()) {
       return authState.pnp.getActor({
         canisterId: CASHIER_BACKEND_CANISTER_ID,
         idl: cashierBackend.idlFactory,
--- src/cashier_frontend_new/src/modules/links/stores/links.svelte.ts
+++ src/cashier_frontend_new/src/modules/links/stores/links.svelte.ts
@@ -1,5 +1,8 @@
 import type { LinkDto } from "$lib/generated/cashier_backend/cashier_backend.did";
 import { managedState } from "$lib/managedState";
+import { authState } from "$modules/auth/state/auth.svelte";
+import { accountState } from "$modules/shared/state/auth.svelte";
+import { derived } from "svelte/store";
 import { cashierBackendService } from "../services/cashierBackend";
 
 // A state for the user tokens list
@@ -7,4 +10,5 @@ export const linkListQuery = managedState<LinkDto[]>({
   queryFn: async () => {
     return (await cashierBackendService.getLinks()).unwrap();
   },
+  effect: true,
 });
