5d1a6dfedf2c9410a2e303a174e3b5d50e67aafd
barchitect <tbng84@gmail.com>
2025-10-01 16:21:08 +0700

    receive token

--- src/cashier_frontend_new/src/routes/wallet/receive/+page.svelte
+++ src/cashier_frontend_new/src/routes/wallet/receive/+page.svelte
@@ -2,6 +2,11 @@
   import { goto } from "$app/navigation";
   import { resolve } from "$app/paths";
   import Button from '$lib/shadcn/components/ui/button/button.svelte';
+  import { accountState } from '$modules/shared/state/auth.svelte';
+  import { walletTokensQuery } from "$modules/token/state/tokenStore.svelte";
+  import { Principal } from '@dfinity/principal';
+
+  let selectedToken: Principal = $state(Principal.anonymous());
 </script>
 
 <div class="py-6">
@@ -11,4 +16,18 @@
 <div>
   <h2>Receive Tokens</h2>
   <p>Select a token to receive:</p>
+  <select bind:value={selectedToken}>
+    {#if walletTokensQuery.data}
+      {#each walletTokensQuery.data as token (token.address)}
+        {#if token.enabled}
+          <option value={token.address}>{token.symbol} - {token.name}</option>
+        {/if}
+      {/each}
+    {:else}
+      <option disabled>No tokens available</option>
+    {/if}
+  </select>
+  <p>Receive address</p>
+  <p><strong>{ accountState.account?.owner }</strong></p>
+
 </div>
