1fa828632a283cd3c418817d398f0edf4ba7710b
Francesco <ufoscout@gmail.com>
2025-10-03 16:05:54 +0200

    Add Protected component to protect routes and components

---
+++ src/cashier_frontend_new/src/routes/wallet/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Protected from "$modules/shared/components/Protected.svelte";
+  let { children } = $props();
+</script>
+
+<Protected>
+  {@render children?.()}
+</Protected>
+
