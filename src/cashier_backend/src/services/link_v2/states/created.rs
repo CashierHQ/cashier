54b7827dabd11b82d0460d86f0edb6ef3aacd3ba
barchitect <tbng84@gmail.com>
2025-10-13 14:56:35 +0700

    link_v2 initial

---
+++ src/cashier_backend/src/services/link_v2/states/created.rs
@@ -0,0 +1,24 @@
+use crate::services::link_v2::{states::active::ActiveState, traits::LinkV2State};
+use cashier_backend_types::repository::link::v1::{Link, LinkState};
+use std::{fmt::Debug, future::Future, pin::Pin};
+
+#[derive(Debug)]
+pub struct CreatedState {
+    pub link: Link,
+}
+
+impl CreatedState {
+    pub fn new(link: &Link) -> Self {
+        Self { link: link.clone() }
+    }
+}
+
+impl LinkV2State for CreatedState {
+    fn go_next(&self) -> Pin<Box<dyn Future<Output = Result<Box<dyn LinkV2State>, String>>>> {
+        let mut link_clone = self.link.clone();
+        Box::pin(async move {
+            link_clone.state = LinkState::Active;
+            Ok(Box::new(ActiveState::new(&link_clone)) as Box<dyn LinkV2State>)
+        })
+    }
+}
