124517748ccdb0d78d0ecdd3bc3077e93d16ae3c
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-10-14 13:29:45 +0700

    add drawer

--- src/cashier_frontend_new/src/modules/links/types/action/intentTask.ts
+++ src/cashier_frontend_new/src/modules/links/types/action/intentTask.ts
@@ -1,5 +1,6 @@
 import type { IntentTask as BackendIntentTask } from "$lib/generated/cashier_backend/cashier_backend.did";
 import { rsMatch } from "$lib/rsMatch";
+import { assertUnreachable } from "$lib/rsMatch";
 
 export class IntentTask {
   private constructor(public readonly id: string) {}
@@ -15,6 +16,27 @@ export class IntentTask {
       TransferLinkToWallet: () => IntentTask.TransferLinkToWallet,
       TransferWalletToTreasury: () => IntentTask.TransferWalletToTreasury,
     });
+  }
+
+  /**
+   * Exhaustive matching for IntentTask instances
+   * TypeScript will ensure all cases are handled
+   */
+  match<T>(matcher: {
+    TransferWalletToLink: () => T;
+    TransferLinkToWallet: () => T;
+    TransferWalletToTreasury: () => T;
+  }): T {
+    switch (this.id) {
+      case IntentTask.TransferWalletToLink.id:
+        return matcher.TransferWalletToLink();
+      case IntentTask.TransferLinkToWallet.id:
+        return matcher.TransferLinkToWallet();
+      case IntentTask.TransferWalletToTreasury.id:
+        return matcher.TransferWalletToTreasury();
+      default:
+        return assertUnreachable(this as never);
+    }
   }
 }
 
