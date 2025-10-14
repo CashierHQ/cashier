cd73d3fccb62e29c634a9eb363287c79b89d9977
barchitect <tbng84@gmail.com>
2025-10-14 13:00:00 +0700

    fix lint errors

--- src/cashier_backend/src/link_v2/traits.rs
+++ src/cashier_backend/src/link_v2/traits.rs
@@ -1,14 +1,10 @@
 use candid::Principal;
 use cashier_backend_types::{
     error::CanisterError,
-    repository::{
-        action::v1::{Action, ActionType},
-        intent::v2::Intent,
-        link::v1::Link,
-        transaction::v2::Transaction,
-    },
+    link_v2::CreateActionResult,
+    repository::{action::v1::ActionType, link::v1::Link},
 };
-use std::{collections::HashMap, fmt::Debug, future::Future, pin::Pin};
+use std::{fmt::Debug, future::Future, pin::Pin};
 
 pub trait LinkV2: Debug {
     fn get_link_model(&self) -> Link;
@@ -18,33 +14,15 @@ pub trait LinkV2: Debug {
         caller: Principal,
         action: ActionType,
         created_at_ts: u64,
-    ) -> Result<(Action, Vec<Intent>, HashMap<String, Vec<Transaction>>), CanisterError>;
+    ) -> Result<CreateActionResult, CanisterError>;
 
     fn publish(&self) -> Pin<Box<dyn Future<Output = Result<Link, CanisterError>>>> {
         Box::pin(async move { Err(CanisterError::from("publish not implemented")) })
-    }
-    fn unpublish(&self) -> Pin<Box<dyn Future<Output = Result<Link, CanisterError>>>> {
-        Box::pin(async move { Err(CanisterError::from("unpublish not implemented")) })
-    }
-    fn claim(&self) -> Pin<Box<dyn Future<Output = Result<Link, CanisterError>>>> {
-        Box::pin(async move { Err(CanisterError::from("claim not implemented")) })
-    }
-    fn withdraw(&self) -> Pin<Box<dyn Future<Output = Result<Link, CanisterError>>>> {
-        Box::pin(async move { Err(CanisterError::from("withdraw not implemented")) })
     }
 }
 
 pub trait LinkV2State: Debug {
     fn publish(&self) -> Pin<Box<dyn Future<Output = Result<Link, CanisterError>>>> {
         Box::pin(async move { Err(CanisterError::from("publish not implemented")) })
-    }
-    fn unpublish(&self) -> Pin<Box<dyn Future<Output = Result<Link, CanisterError>>>> {
-        Box::pin(async move { Err(CanisterError::from("unpublish not implemented")) })
-    }
-    fn claim(&self) -> Pin<Box<dyn Future<Output = Result<Link, CanisterError>>>> {
-        Box::pin(async move { Err(CanisterError::from("claim not implemented")) })
-    }
-    fn withdraw(&self) -> Pin<Box<dyn Future<Output = Result<Link, CanisterError>>>> {
-        Box::pin(async move { Err(CanisterError::from("withdraw not implemented")) })
     }
 }
---
+++ src/cashier_backend_types/src/link_v2/mod.rs
@@ -0,0 +1,9 @@
+use crate::repository::{action::v1::Action, intent::v2::Intent, transaction::v2::Transaction};
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+pub struct CreateActionResult {
+    pub action: Action,
+    pub intents: Vec<Intent>,
+    pub intent_txs_map: HashMap<String, Vec<Transaction>>,
+}
