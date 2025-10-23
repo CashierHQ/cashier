9e49db73f1f6cc9471ca33b13b20200d44592314
barchitect <tbng84@gmail.com>
2025-10-23 20:22:35 +0700

    export link_v2 dto

--- src/cashier_backend/src/link_v2/services/link_service.rs
+++ src/cashier_backend/src/link_v2/services/link_service.rs
@@ -6,6 +6,7 @@ use crate::services::action::ActionService;
 use crate::{link_v2::links::factory, repositories};
 use candid::Principal;
 use cashier_backend_types::dto::action::IntentDto;
+use cashier_backend_types::link_v2::dto::LinkActionDto;
 use cashier_backend_types::repository::link::v1::LinkState;
 use cashier_backend_types::{
     dto::{
@@ -49,7 +50,7 @@ impl<R: Repositories> LinkV2Service<R> {
         canister_id: Principal,
         input: CreateLinkInput,
         created_at_ts: u64,
-    ) -> Result<GetLinkResp, CanisterError> {
+    ) -> Result<LinkActionDto, CanisterError> {
         let link_model = factory::create_link(creator_id, input, created_at_ts, canister_id)?;
 
         // save link & user_link to db
@@ -70,9 +71,11 @@ impl<R: Repositories> LinkV2Service<R> {
             )
             .await?;
 
-        Ok(GetLinkResp {
-            link: LinkDto::from(link_model),
-            action: Some(action_dto),
+        let link_dto = LinkDto::from(link_model);
+
+        Ok(LinkActionDto {
+            link: link_dto,
+            action: action_dto,
         })
     }
 
@@ -97,50 +100,6 @@ impl<R: Repositories> LinkV2Service<R> {
         self.link_repository.update(link.clone());
 
         Ok(LinkDto::from(link))
-    }
-
-    /// Activate a link, changing its state to ACTIVE
-    /// Only the creator of the link can activate it
-    /// # Deprecated:
-    /// This function is deprecated and will be replaced by the `process_action_v2` function with a CREATE_LINK action.
-    ///
-    /// # Arguments
-    /// * `caller` - The principal of the user attempting to activate the link
-    /// * `link_id` - The ID of the link to be activated
-    /// # Returns
-    /// * `LinkDto` - The DTO of the activated link
-    /// # Errors
-    /// * `CanisterError` - If the link is not found, the caller is not the creator, or if there is an error during activation
-    pub async fn activate_link(
-        &mut self,
-        caller: Principal,
-        link_id: &str,
-        canister_id: Principal,
-    ) -> Result<LinkDto, CanisterError> {
-        let link = self
-            .link_repository
-            .get(&link_id.to_string())
-            .ok_or_else(|| CanisterError::NotFound("Link not found".to_string()))?;
-
-        if link.creator != caller {
-            return Err(CanisterError::Unauthorized(
-                "Only the creator can publish the link".to_string(),
-            ));
-        }
-
-        let action_id = format!("activate-{}", link.id); // TODO
-        let action = self
-            .action_service
-            .get_action_by_id(&action_id)
-            .ok_or_else(|| CanisterError::NotFound("Action not found".to_string()))?;
-
-        let link = factory::from_link(link, canister_id)?;
-        let activate_result = link.process_action(caller, action).await?;
-
-        // update link in db
-        self.link_repository.update(activate_result.link.clone());
-
-        Ok(LinkDto::from(activate_result.link))
     }
 
     pub async fn create_action(
@@ -185,7 +144,7 @@ impl<R: Repositories> LinkV2Service<R> {
         caller: Principal,
         canister_id: Principal,
         action_id: &str,
-    ) -> Result<ActionDto, CanisterError> {
+    ) -> Result<LinkActionDto, CanisterError> {
         let action = self
             .action_service
             .get_action_by_id(action_id)
@@ -208,6 +167,11 @@ impl<R: Repositories> LinkV2Service<R> {
             None,
         );
 
-        Ok(action_dto)
+        let link_dto = LinkDto::from(result.link);
+
+        Ok(LinkActionDto {
+            link: link_dto,
+            action: action_dto,
+        })
     }
 }
