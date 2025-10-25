3f92998b1243053b307b05f3a1111cd5ff67d358
barchitect <tbng84@gmail.com>
2025-10-25 09:08:17 +0700

    rename receiver

--- src/integration_tests/tests/cashier_backend/link_v2/send_tip/receive_link_v2.rs
+++ src/integration_tests/tests/cashier_backend/link_v2/send_tip/receive_link_v2.rs
@@ -26,8 +26,8 @@ async fn it_should_fail_receive_icp_token_tip_linkv2_if_link_not_active() {
         let (creator_fixture, create_link_result) =
             create_tip_linkv2_fixture(ctx, ICP_TOKEN, tip_amount).await;
 
-        let claimer = TestUser::User2.get_principal();
-        let claimer_fixture = LinkTestFixture::new(creator_fixture.ctx.clone(), &claimer).await;
+        let receiver = TestUser::User2.get_principal();
+        let receiver_fixture = LinkTestFixture::new(creator_fixture.ctx.clone(), &receiver).await;
 
         // Act: create RECEIVE action
         let link_id = create_link_result.link.id.clone();
@@ -35,7 +35,7 @@ async fn it_should_fail_receive_icp_token_tip_linkv2_if_link_not_active() {
             link_id: link_id.clone(),
             action_type: ActionType::Receive,
         };
-        let create_action_result = claimer_fixture.create_action_v2(create_action_input).await;
+        let create_action_result = receiver_fixture.create_action_v2(create_action_input).await;
 
         // Assert: action created successfully
         assert!(create_action_result.is_err());
@@ -63,8 +63,8 @@ async fn it_should_fail_receive_icp_token_tip_linkv2_if_requested_more_than_once
         let (creator_fixture, create_link_result) =
             activate_tip_link_v2_fixture(ctx, ICP_TOKEN, tip_amount).await;
 
-        let claimer = TestUser::User2.get_principal();
-        let claimer_fixture = LinkTestFixture::new(creator_fixture.ctx.clone(), &claimer).await;
+        let receiver = TestUser::User2.get_principal();
+        let receiver_fixture = LinkTestFixture::new(creator_fixture.ctx.clone(), &receiver).await;
 
         // Act: create RECEIVE action
         let link_id = create_link_result.link.id.clone();
@@ -72,13 +72,13 @@ async fn it_should_fail_receive_icp_token_tip_linkv2_if_requested_more_than_once
             link_id: link_id.clone(),
             action_type: ActionType::Receive,
         };
-        let create_action_result = claimer_fixture.create_action_v2(create_action_input).await;
+        let create_action_result = receiver_fixture.create_action_v2(create_action_input).await;
 
         // Act: process RECEIVE action
         let action_dto = create_action_result.unwrap();
         let action_id = action_dto.id.clone();
         let process_action_input = ProcessActionV2Input { action_id };
-        let _process_action_result = claimer_fixture
+        let _process_action_result = receiver_fixture
             .process_action_v2(process_action_input)
             .await;
 
@@ -88,7 +88,7 @@ async fn it_should_fail_receive_icp_token_tip_linkv2_if_requested_more_than_once
             link_id: link_id.clone(),
             action_type: ActionType::Receive,
         };
-        let create_action_result = claimer_fixture.create_action_v2(create_action_input).await;
+        let create_action_result = receiver_fixture.create_action_v2(create_action_input).await;
 
         // Assert: action creation failed
         assert!(create_action_result.is_err());
@@ -113,16 +113,16 @@ async fn it_should_succeed_receive_icp_token_tip_linkv2() {
         let (creator_fixture, create_link_result) =
             activate_tip_link_v2_fixture(ctx, ICP_TOKEN, tip_amount).await;
 
-        let claimer = TestUser::User2.get_principal();
-        let claimer_fixture = LinkTestFixture::new(creator_fixture.ctx.clone(), &claimer).await;
+        let receiver = TestUser::User2.get_principal();
+        let receiver_fixture = LinkTestFixture::new(creator_fixture.ctx.clone(), &receiver).await;
 
-        let claimer_account = Account {
-            owner: claimer,
+        let receiver_account = Account {
+            owner: receiver,
             subaccount: None,
         };
-        let icp_ledger_client = ctx.new_icp_ledger_client(claimer);
+        let icp_ledger_client = ctx.new_icp_ledger_client(receiver);
         let icp_balance_before = icp_ledger_client
-            .balance_of(&claimer_account)
+            .balance_of(&receiver_account)
             .await
             .unwrap();
 
@@ -132,7 +132,7 @@ async fn it_should_succeed_receive_icp_token_tip_linkv2() {
             link_id: link_id.clone(),
             action_type: ActionType::Receive,
         };
-        let create_action_result = claimer_fixture.create_action_v2(create_action_input).await;
+        let create_action_result = receiver_fixture.create_action_v2(create_action_input).await;
 
         // Assert: action created successfully
         assert!(create_action_result.is_ok());
@@ -146,7 +146,7 @@ async fn it_should_succeed_receive_icp_token_tip_linkv2() {
         assert_eq!(intent1.task, IntentTask::TransferLinkToWallet);
         match intent1.r#type {
             IntentType::Transfer(ref transfer) => {
-                assert_eq!(transfer.to, Wallet::new(claimer));
+                assert_eq!(transfer.to, Wallet::new(receiver));
                 assert_eq!(transfer.from, link_id_to_account(ctx, &link_id).into());
                 assert_eq!(
                     transfer.amount,
@@ -160,7 +160,7 @@ async fn it_should_succeed_receive_icp_token_tip_linkv2() {
         let tx0 = &intent1.transactions[0];
         match tx0.protocol {
             Protocol::IC(IcTransaction::Icrc1Transfer(ref data)) => {
-                assert_eq!(data.to, Wallet::new(claimer));
+                assert_eq!(data.to, Wallet::new(receiver));
                 assert_eq!(data.from, link_id_to_account(ctx, &link_id).into());
                 assert_eq!(
                     data.amount,
@@ -175,7 +175,7 @@ async fn it_should_succeed_receive_icp_token_tip_linkv2() {
         let process_action_input = ProcessActionV2Input {
             action_id: create_action_result.id.clone(),
         };
-        let process_action_result = claimer_fixture
+        let process_action_result = receiver_fixture
             .process_action_v2(process_action_input)
             .await;
 
@@ -196,7 +196,7 @@ async fn it_should_succeed_receive_icp_token_tip_linkv2() {
 
         // Assert: receiveer's ICP balance increased
         let icp_balance_after = icp_ledger_client
-            .balance_of(&claimer_account)
+            .balance_of(&receiver_account)
             .await
             .unwrap();
         assert_eq!(
@@ -206,7 +206,7 @@ async fn it_should_succeed_receive_icp_token_tip_linkv2() {
         );
 
         // Assert: link's account balance is zero
-        let link_account = link_id_to_account(&claimer_fixture.ctx, &link_id);
+        let link_account = link_id_to_account(&receiver_fixture.ctx, &link_id);
         let link_balance = icp_ledger_client.balance_of(&link_account).await.unwrap();
         assert_eq!(
             link_balance,
@@ -228,17 +228,17 @@ async fn it_should_succeed_receive_icrc_token_tip_linkv2() {
         let (creator_fixture, create_link_result) =
             activate_tip_link_v2_fixture(ctx, CKBTC_ICRC_TOKEN, tip_amount).await;
 
-        let claimer = TestUser::User2.get_principal();
-        let claimer_fixture = LinkTestFixture::new(creator_fixture.ctx.clone(), &claimer).await;
+        let receiver = TestUser::User2.get_principal();
+        let receiver_fixture = LinkTestFixture::new(creator_fixture.ctx.clone(), &receiver).await;
 
-        let claimer_account = Account {
-            owner: claimer,
+        let receiver_account = Account {
+            owner: receiver,
             subaccount: None,
         };
-        let ckbtc_ledger_client = ctx.new_icrc_ledger_client(CKBTC_ICRC_TOKEN, claimer);
+        let ckbtc_ledger_client = ctx.new_icrc_ledger_client(CKBTC_ICRC_TOKEN, receiver);
 
         let ckbtc_balance_before = ckbtc_ledger_client
-            .balance_of(&claimer_account)
+            .balance_of(&receiver_account)
             .await
             .unwrap();
 
@@ -248,7 +248,7 @@ async fn it_should_succeed_receive_icrc_token_tip_linkv2() {
             link_id: link_id.clone(),
             action_type: ActionType::Receive,
         };
-        let create_action_result = claimer_fixture.create_action_v2(create_action_input).await;
+        let create_action_result = receiver_fixture.create_action_v2(create_action_input).await;
 
         // Assert: action created successfully
         assert!(create_action_result.is_ok());
@@ -262,7 +262,7 @@ async fn it_should_succeed_receive_icrc_token_tip_linkv2() {
         assert_eq!(intent1.task, IntentTask::TransferLinkToWallet);
         match intent1.r#type {
             IntentType::Transfer(ref transfer) => {
-                assert_eq!(transfer.to, Wallet::new(claimer));
+                assert_eq!(transfer.to, Wallet::new(receiver));
                 assert_eq!(transfer.from, link_id_to_account(ctx, &link_id).into());
                 assert_eq!(
                     transfer.amount,
@@ -276,7 +276,7 @@ async fn it_should_succeed_receive_icrc_token_tip_linkv2() {
         let tx0 = &intent1.transactions[0];
         match tx0.protocol {
             Protocol::IC(IcTransaction::Icrc1Transfer(ref data)) => {
-                assert_eq!(data.to, Wallet::new(claimer));
+                assert_eq!(data.to, Wallet::new(receiver));
                 assert_eq!(data.from, link_id_to_account(ctx, &link_id).into());
                 assert_eq!(
                     data.amount,
@@ -291,7 +291,7 @@ async fn it_should_succeed_receive_icrc_token_tip_linkv2() {
         let process_action_input = ProcessActionV2Input {
             action_id: create_action_result.id.clone(),
         };
-        let process_action_result = claimer_fixture
+        let process_action_result = receiver_fixture
             .process_action_v2(process_action_input)
             .await;
 
@@ -312,7 +312,7 @@ async fn it_should_succeed_receive_icrc_token_tip_linkv2() {
 
         // Assert: receiveer's CKBTC balance increased
         let ckbtc_balance_after = ckbtc_ledger_client
-            .balance_of(&claimer_account)
+            .balance_of(&receiver_account)
             .await
             .unwrap();
         assert_eq!(
@@ -322,7 +322,7 @@ async fn it_should_succeed_receive_icrc_token_tip_linkv2() {
         );
 
         // Assert: link's account balance is zero
-        let link_account = link_id_to_account(&claimer_fixture.ctx, &link_id);
+        let link_account = link_id_to_account(&receiver_fixture.ctx, &link_id);
         let link_balance = ckbtc_ledger_client.balance_of(&link_account).await.unwrap();
         assert_eq!(
             link_balance,
