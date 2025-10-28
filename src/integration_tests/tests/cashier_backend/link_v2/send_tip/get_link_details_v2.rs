1804a7114b0c9545cc29d42303d759889335918a
barchitect <tbng84@gmail.com>
2025-10-28 14:08:41 +0700

    add transactions and icrc112 to action_dto in get_link_details

--- src/integration_tests/tests/cashier_backend/link_v2/send_tip/get_link_details_v2.rs
+++ src/integration_tests/tests/cashier_backend/link_v2/send_tip/get_link_details_v2.rs
@@ -1,12 +1,15 @@
 // Copyright (c) 2025 Cashier Protocol Labs
 // Licensed under the MIT License (see LICENSE file in the project root)
 
-use crate::cashier_backend::link::fixture::{LinkTestFixture, activate_tip_link_v2_fixture};
+use crate::cashier_backend::link::fixture::{
+    LinkTestFixture, activate_tip_link_v2_fixture, create_tip_linkv2_fixture,
+};
+use crate::constant::ICP_PRINCIPAL;
 use crate::utils::link_id_to_account::link_id_to_account;
 use crate::utils::principal::TestUser;
 use crate::utils::with_pocket_ic_context;
-use candid::Nat;
-use cashier_backend_types::constant::ICP_TOKEN;
+use candid::{Nat, Principal};
+use cashier_backend_types::constant::{FEE_TREASURY_PRINCIPAL, ICP_TOKEN};
 use cashier_backend_types::dto::action::CreateActionInput;
 use cashier_backend_types::dto::link::GetLinkOptions;
 use cashier_backend_types::error::CanisterError;
@@ -14,6 +17,8 @@ use cashier_backend_types::repository::action::v1::{ActionState, ActionType};
 use cashier_backend_types::repository::common::Wallet;
 use cashier_backend_types::repository::intent::v1::{IntentState, IntentTask, IntentType};
 use cashier_backend_types::repository::link::v1::LinkState;
+use cashier_backend_types::repository::transaction::v1::{IcTransaction, Protocol};
+use cashier_common::constant::CREATE_LINK_FEE;
 
 #[tokio::test]
 async fn it_should_fail_get_tip_linkv2_details_if_link_not_found() {
@@ -72,7 +77,7 @@ async fn it_should_succeed_get_tip_linkv2_details_with_no_option() {
 }
 
 #[tokio::test]
-async fn it_should_succeed_get_linkv2_details_with_create_action_type() {
+async fn it_should_succeed_get_linkv2_details_with_create_action_succeeded() {
     with_pocket_ic_context::<_, ()>(async move |ctx| {
         // Arrange
         let tip_amount = 1_000_000u64;
@@ -133,6 +138,117 @@ async fn it_should_succeed_get_linkv2_details_with_option_action_not_existent() 
         assert_eq!(link.id, link_id);
         assert_eq!(link.state, LinkState::Active);
         assert!(get_link_result.action.is_none());
+
+        Ok(())
+    })
+    .await
+    .unwrap();
+}
+
+#[tokio::test]
+async fn it_should_succeed_get_linkv2_details_with_create_action() {
+    with_pocket_ic_context::<_, ()>(async move |ctx| {
+        // Arrange
+        let caller = TestUser::User1.get_principal();
+        let tip_amount = 1_000_000u64;
+        let (test_fixture, create_link_result) =
+            create_tip_linkv2_fixture(ctx, ICP_TOKEN, tip_amount).await;
+
+        // Act
+        let link_id = create_link_result.link.id;
+        let option = GetLinkOptions {
+            action_type: ActionType::CreateLink,
+        };
+        let get_links_result = test_fixture
+            .get_link_details_v2(&link_id, Some(option))
+            .await;
+
+        // Assert
+        assert!(get_links_result.is_ok());
+        let get_link_result = get_links_result.unwrap();
+        let link = get_link_result.link;
+        assert_eq!(link.id, link_id);
+        assert_eq!(link.state, LinkState::CreateLink);
+        let action = get_link_result.action.unwrap();
+        assert_eq!(action.r#type, ActionType::CreateLink);
+        assert_eq!(action.state, ActionState::Created);
+        assert_eq!(action.intents.len(), 2);
+
+        // Assert Intent1 TransferWalletToLink
+        let intent1 = &action.intents[0];
+        assert_eq!(intent1.state, IntentState::Created);
+        match intent1.r#type {
+            IntentType::Transfer(ref transfer) => {
+                assert_eq!(transfer.from, Wallet::new(caller));
+                assert_eq!(transfer.to, link_id_to_account(ctx, &link.id).into());
+            }
+            _ => panic!("Expected Transfer intent type"),
+        }
+        assert_eq!(intent1.transactions.len(), 1);
+        let tx0 = &intent1.transactions[0];
+        match tx0.protocol {
+            Protocol::IC(IcTransaction::Icrc1Transfer(ref data)) => {
+                assert_eq!(data.from, Wallet::new(caller));
+                assert_eq!(data.to, link_id_to_account(ctx, &link.id).into());
+            }
+            _ => panic!("Expected Icrc1Transfer transaction"),
+        }
+
+        // Assert Intent2 TransferWalletToTreasury
+        let intent2 = &action.intents[1];
+        assert_eq!(intent2.state, IntentState::Created);
+        assert_eq!(intent2.task, IntentTask::TransferWalletToTreasury);
+        match intent2.r#type {
+            IntentType::TransferFrom(ref transfer_from) => {
+                assert_eq!(transfer_from.from, Wallet::new(caller));
+                assert_eq!(transfer_from.to, Wallet::new(FEE_TREASURY_PRINCIPAL));
+            }
+            _ => panic!("Expected TransferFrom intent type"),
+        }
+        assert_eq!(intent2.transactions.len(), 2);
+        let tx1 = &intent2.transactions[0];
+        match tx1.protocol {
+            Protocol::IC(IcTransaction::Icrc2Approve(ref data)) => {
+                assert_eq!(data.from, Wallet::new(caller));
+                assert_eq!(data.spender, Wallet::new(ctx.cashier_backend_principal));
+            }
+            _ => panic!("Expected Icrc2Approve transaction"),
+        }
+        let tx2 = &intent2.transactions[1];
+        match tx2.protocol {
+            Protocol::IC(IcTransaction::Icrc2TransferFrom(ref data)) => {
+                assert_eq!(data.from, Wallet::new(caller));
+                assert_eq!(data.to, Wallet::new(FEE_TREASURY_PRINCIPAL));
+                assert_eq!(data.spender, Wallet::new(ctx.cashier_backend_principal));
+                assert_eq!(data.amount, Nat::from(CREATE_LINK_FEE));
+            }
+            _ => panic!("Expected Icrc2TransferFrom transaction"),
+        }
+
+        // Assert ICRC-112 requests
+        assert!(action.icrc_112_requests.is_some());
+        let icrc112_requests = action.icrc_112_requests.unwrap();
+        assert_eq!(icrc112_requests.len(), 1);
+        let requests = &icrc112_requests[0];
+
+        assert_eq!(requests.len(), 2);
+        for req in requests {
+            match req.method.as_str() {
+                "icrc1_transfer" => {
+                    assert_eq!(
+                        req.canister_id,
+                        Principal::from_text(ICP_PRINCIPAL).unwrap()
+                    );
+                }
+                "icrc2_approve" => {
+                    assert_eq!(
+                        req.canister_id,
+                        Principal::from_text(ICP_PRINCIPAL).unwrap()
+                    );
+                }
+                _ => panic!("Unexpected method in ICRC-112 request"),
+            }
+        }
 
         Ok(())
     })
@@ -202,6 +318,15 @@ async fn it_should_succeed_get_linkv2_details_with_receive_action() {
             }
             _ => panic!("Expected Transfer intent type"),
         }
+        assert_eq!(intent1.transactions.len(), 1);
+        let tx0 = &intent1.transactions[0];
+        match tx0.protocol {
+            Protocol::IC(IcTransaction::Icrc1Transfer(ref data)) => {
+                assert_eq!(data.to, Wallet::new(receiver));
+                assert_eq!(data.from, link_id_to_account(ctx, &link_id).into());
+            }
+            _ => panic!("Expected Icrc1Transfer transaction"),
+        }
 
         Ok(())
     })
@@ -266,6 +391,15 @@ async fn it_should_succeed_get_linkv2_details_with_withdraw_action() {
                 assert_eq!(transfer.from, link_id_to_account(ctx, &link_id).into());
             }
             _ => panic!("Expected Transfer intent type"),
+        }
+        assert_eq!(intent1.transactions.len(), 1);
+        let tx0 = &intent1.transactions[0];
+        match tx0.protocol {
+            Protocol::IC(IcTransaction::Icrc1Transfer(ref data)) => {
+                assert_eq!(data.to, Wallet::new(caller));
+                assert_eq!(data.from, link_id_to_account(ctx, &link_id).into());
+            }
+            _ => panic!("Expected Icrc1Transfer transaction"),
         }
 
         Ok(())
