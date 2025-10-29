478de786e6d9a68808cc89e8ebca8a4fa1a567b1
barchitect <tbng84@gmail.com>
2025-10-29 18:37:16 +0700

    fix processing price if fetching fails

--- src/cashier_frontend_new/src/modules/token/state/tokenPriceStore.svelte.ts
+++ src/cashier_frontend_new/src/modules/token/state/tokenPriceStore.svelte.ts
@@ -25,7 +25,15 @@ export class TokenPriceStore {
           const result = prices_results[i];
 
           if (result.status === "fulfilled") {
-            Object.assign(prices, result.value.unwrap());
+            if (result.value.isOk()) {
+              Object.assign(prices, result.value.unwrap());
+            } else {
+              console.error(
+                "Failed to fetch token prices:",
+                result.value.unwrapErr(),
+                services[i],
+              );
+            }
           } else {
             console.error(
               "Failed to fetch token prices:",
@@ -37,7 +45,7 @@ export class TokenPriceStore {
 
         return prices;
       },
-      refetchInterval: 60_000, // Refresh every 60 seconds to keep prices up-to-date
+      refetchInterval: 15_000, // Refresh every 60 seconds to keep prices up-to-date
       persistedKey: ["tokenPricesQuery"],
       storageType: "localStorage",
     });
