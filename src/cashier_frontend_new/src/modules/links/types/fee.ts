f4f761b0b802565381022d79296463c6d17d2631
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-10-31 13:58:45 +0700

    move files

--- src/cashier_frontend_new/src/modules/links/types/fee.ts
+++ src/cashier_frontend_new/src/modules/links/types/fee.ts
@@ -2,6 +2,30 @@ import type { ActionType } from "$modules/links/types/action/actionType";
 import type Intent from "$modules/links/types/action/intent";
 
 /**
+ * Enumeration of fee types
+ */
+export class FeeType {
+  static readonly NETWORK_FEE = "NETWORK_FEE";
+  static readonly CREATE_LINK_FEE = "CREATE_LINK_FEE";
+}
+
+/**
+ * Fee item representation
+ */
+export interface FeeItem {
+  feeType: FeeType;
+
+  symbol: string;
+  price?: number;
+  /** in formmated string */
+  amount: string;
+  /** in formmated string */
+  usdValueStr?: string;
+  /** number usd value */
+  usdValue?: number;
+}
+
+/**
  * Input type for computeAmountAndFee function in FeeService
  */
 export type ComputeAmountAndFeeInput = {
