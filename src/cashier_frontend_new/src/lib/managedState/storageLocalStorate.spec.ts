499a4c9ae6cafb4bb2e5433dc54b289352872197
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-11-05 23:35:24 +0700

    add serde config for managedState

--- src/cashier_frontend_new/src/lib/managedState/storageLocalStorate.spec.ts
+++ src/cashier_frontend_new/src/lib/managedState/storageLocalStorate.spec.ts
@@ -5,6 +5,85 @@
 import { beforeEach, describe, expect, it } from "vitest";
 import * as devalue from "devalue";
 import { LocalStorageStore } from "./storageLocalStorage";
+import { Principal } from "@dfinity/principal";
+import type { DevalueSerde } from ".";
+import { Ed25519KeyIdentity } from "@dfinity/identity";
+
+class TestValue {
+  public name: string;
+  public age: number;
+  public principal: Principal;
+  public testers: Principal[];
+  public option: Option;
+  constructor({
+    name,
+    age,
+    principal,
+    testers,
+    option,
+  }: {
+    name: string;
+    age: number;
+    principal: Principal;
+    testers: Principal[];
+    option: Option;
+  }) {
+    this.name = name;
+    this.age = age;
+    this.principal = principal;
+    this.testers = testers;
+    this.option = option;
+  }
+}
+
+export class Option {
+  private constructor() {}
+
+  static readonly OPTION_A = "OPTION_A";
+  static readonly OPTION_B = "OPTION_B";
+  static readonly OPTION_C = "OPTION_C";
+  static readonly OPTION_D = "OPTION_D";
+}
+
+export type OptionValue =
+  | typeof Option.OPTION_A
+  | typeof Option.OPTION_B
+  | typeof Option.OPTION_C
+  | typeof Option.OPTION_D;
+
+const testValueDevalueSerde: DevalueSerde = {
+  serialize: {
+    TestValue: (v) =>
+      v instanceof TestValue && {
+        name: v.name,
+        age: v.age,
+        principal: v.principal,
+        testers: v.testers,
+        option: v.option,
+      },
+    Principal: (principal) =>
+      principal instanceof Principal && principal.toText(),
+  },
+  deserialize: {
+    TestValue: (v) => {
+      const value = v as {
+        name: string;
+        age: number;
+        principal: Principal;
+        testers: Principal[];
+        option: string;
+      };
+      return new TestValue({
+        name: value.name,
+        age: value.age,
+        principal: value.principal,
+        testers: value.testers,
+        option: value.option,
+      });
+    },
+    Principal: (data) => typeof data == "string" && Principal.fromText(data),
+  },
+};
 
 describe("LocalStorageStore", () => {
   beforeEach(() => {
@@ -96,5 +175,49 @@ describe("LocalStorageStore", () => {
 
     // Assert
     expect(store.getItem()).toEqual(null);
+  });
+
+  it("should handle custom values", () => {
+    // Arrange
+    const store = new LocalStorageStore("test_key", testValueDevalueSerde);
+    store.removeItem();
+
+    const item = {
+      owner: Principal.fromText("rrkah-fqaaa-aaaaa-aaaaq-cai"),
+      deepnested: {
+        test: [
+          new TestValue({
+            name: "Bob",
+            age: 25,
+            principal: Principal.fromText("rrkah-fqaaa-aaaaa-aaaaq-cai"),
+            testers: [
+              Ed25519KeyIdentity.generate().getPrincipal(),
+              Ed25519KeyIdentity.generate().getPrincipal(),
+              Ed25519KeyIdentity.generate().getPrincipal(),
+            ],
+            option: Option.OPTION_B,
+          }),
+        ],
+        testBigint: 42n,
+      },
+      nested: new TestValue({
+        name: "Alice123",
+        age: 30,
+        principal: Principal.fromText(
+          "ur2tx-mciqf-h4p4b-qggnv-arpsc-s3wui-2blbn-aphly-wzoos-iqjik-hae",
+        ),
+        testers: [
+          Ed25519KeyIdentity.generate().getPrincipal(),
+          Ed25519KeyIdentity.generate().getPrincipal(),
+          Ed25519KeyIdentity.generate().getPrincipal(),
+        ],
+        option: Option.OPTION_C,
+      }),
+    };
+    // Act
+    store.setItem(item);
+
+    // Assert
+    expect(store.getItem()).toEqual(item);
   });
 });
