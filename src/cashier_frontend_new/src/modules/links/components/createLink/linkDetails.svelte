e385ea09f869655ef21bb3c6a9c821b4c8059941
barchitect <tbng84@gmail.com>
2025-11-06 18:09:55 +0700

    amount formatted

--- src/cashier_frontend_new/src/modules/links/components/createLink/linkDetails.svelte
+++ src/cashier_frontend_new/src/modules/links/components/createLink/linkDetails.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import { resolve } from "$app/paths";
   import { parseBalanceUnits } from '$modules/shared/utils/converter';
-  import type { LinkCreationStore } from "../../state/linkCreationStore.svelte";
   import { walletStore } from '$modules/token/state/walletStore.svelte';
+  import type { LinkCreationStore } from "../../state/linkCreationStore.svelte";
 
   const {
     link,
@@ -13,6 +13,27 @@
     errorMessage: string | null;
     successMessage: string | null;
   } = $props();
+
+  let amountPerAsset = $derived.by(() => {
+    if (!link.createLinkData.assets || link.createLinkData.assets.length === 0) {
+      return null;
+    }
+
+    const amountPerAsset: Record<string, number> = {};
+
+    for (const assetInfo of link.createLinkData.assets) {
+      const tokenResult = walletStore.findTokenByAddress(assetInfo.address);
+      if (tokenResult.isErr()) {
+        continue;
+      }
+      const token = tokenResult.unwrap();
+      
+      const amount = parseBalanceUnits(assetInfo.useAmount, token.decimals);
+      amountPerAsset[assetInfo.address] = amount;
+    }
+
+    return amountPerAsset;
+  })
 </script>
 
 <div>
@@ -23,10 +44,12 @@
   </div>
   {#if link.createLinkData.assets}
     {#each link.createLinkData.assets as assetInfo (assetInfo.address)}
+      {#if amountPerAsset }
       <div class="mt-2 p-2 border rounded">
         <div><strong>Asset:</strong> {assetInfo.address}</div>
-        <div><strong>Amount per use action:</strong> {parseBalanceUnits(assetInfo.useAmount)}</div>
+        <div><strong>Amount per use action:</strong> {amountPerAsset[assetInfo.address]}</div>
       </div>
+      {/if}
     {/each}
   {/if}
 
