08825b3f8198811f9459f1f2844ba8594d6995ee
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-11-18 11:32:01 +0700

    feat: Implement serde for LinkAction and related types
    - Added serialization and deserialization logic for LinkAction, Action, Intent, and their respective mappers.
    - Introduced LinkActionMapper to handle the conversion between LinkAction instances and their serialized forms.
    - Enhanced existing mappers (ActionMapper, IntentMapper, etc.) to support serialization and deserialization using devalue.
    - Added tests for the new serialization and deserialization functionality across various types, ensuring proper integration and functionality.
    - Refactored Icrc112Request and Action types to include serde capabilities for better state management.

--- src/cashier_frontend_new/src/modules/detailLink/state/linkDetailStore.svelte.ts
+++ src/cashier_frontend_new/src/modules/detailLink/state/linkDetailStore.svelte.ts
@@ -10,7 +10,10 @@ import type { LinkDetailState } from "./linkDetailStates";
 import { LinkActiveState } from "./linkDetailStates/active";
 import { LinkCreatedState } from "./linkDetailStates/created";
 import { LinkInactiveState } from "./linkDetailStates/inactive";
-import type { LinkAction } from "$modules/links/types/linkAndAction";
+import {
+  LinkActionMapper,
+  type LinkAction,
+} from "$modules/links/types/linkAndAction";
 
 /**
  * Store for link detail
@@ -35,6 +38,8 @@ export class LinkDetailStore {
       },
       watch: true,
       storageType: "localStorage",
+      persistedKey: ["linkDetail", id],
+      serde: LinkActionMapper.serde,
     });
     this.#state = new LinkCreatedState(this);
 
