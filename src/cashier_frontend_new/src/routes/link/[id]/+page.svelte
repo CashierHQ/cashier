3e271460a40541ffb944ab197434598b4229f48c
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-11-19 12:10:14 +0700

    fix the redirect loop

--- src/cashier_frontend_new/src/routes/link/[id]/+page.svelte
+++ src/cashier_frontend_new/src/routes/link/[id]/+page.svelte
@@ -1,29 +1,10 @@
 <script lang="ts">
   import { page } from "$app/state";
-  import { resolve } from "$app/paths";
   import Landing from "$modules/useLink/pages/landing.svelte";
-  import { userProfile } from "$modules/shared/services/userProfile.svelte";
-  import { goto } from "$app/navigation";
 
   const id = page.params.id;
-
-  let isReady: boolean = $state(false);
-  $effect(() => {
-    if (!userProfile.isReady()) return;
-
-    if (userProfile.isLoggedIn() && id) {
-      goto(resolve(`/link/${id}/use`));
-      return;
-    }
-
-    isReady = true;
-  });
 </script>
 
-{#if !isReady}
-  Loading...
-{/if}
-
-{#if id && isReady}
+{#if id}
   <Landing {id} />
 {/if}
