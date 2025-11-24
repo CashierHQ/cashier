1dd830a82bfb17bd967f90527d386fe2e34790a5
barchitect <tbng84@gmail.com>
2025-11-24 13:46:26 +0700

    get basket link tests

--- src/integration_tests/tests/cashier_backend/link_v2/send_basket/mod.rs
+++ src/integration_tests/tests/cashier_backend/link_v2/send_basket/mod.rs
@@ -1,4 +1,11 @@
+// Copyright (c) 2025 Cashier Protocol Labs
+// Licensed under the MIT License (see LICENSE file in the project root)
+
 pub mod activate_link;
 pub mod create_link;
 pub mod disable_link;
 pub mod fixture;
+pub mod get_link_details;
+pub mod get_links;
+pub mod receive_link;
+pub mod withdraw_link;
