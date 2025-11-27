35ee973704a2552265a4695e3c56eccebb1b476c
Maksym Osovitnii <maksym.osovitnii@neptuness.com>
2025-11-27 15:19:16 +0200

    Refactor link route layouts to enhance structure and consistency. Updated +layout.svelte, +page.svelte in create and detail routes to include AppHeader and streamline rendering logic within RouteGuard, ensuring a cohesive user experience across link management pages.

--- src/cashier_frontend_new/src/routes/link/detail/[id]/+page.svelte
+++ src/cashier_frontend_new/src/routes/link/detail/[id]/+page.svelte
@@ -3,6 +3,7 @@
   import { resolve } from "$app/paths";
   import { page } from "$app/state";
   import DetailLink from "$modules/detailLink/pages/detail.svelte";
+  import AppHeader from "$modules/shared/components/AppHeader.svelte";
   import RouteGuard from "$modules/shared/components/guards/RouteGuard.svelte";
   import { GuardType } from "$modules/shared/components/guards/types";
   import { appHeaderStore } from "$modules/shared/state/appHeaderStore.svelte";
@@ -41,5 +42,19 @@
   ]}
   linkId={id}
 >
-  <DetailLink {id} />
+  <div class="flex flex-col min-h-screen sm:bg-lightgreen bg-white">
+    <AppHeader isCreateOrEditPage={true} />
+
+    <div class="flex-1 sm:py-4 pb-2 flex items-center justify-center flex-col">
+      <div
+        class="w-full sm:max-w-[600px] max-w-full sm:p-8 px-4 grow-1 bg-white sm:rounded-xl flex flex-col h-full"
+      >
+        <div
+          class="sm:max-h-[calc(100vh-156px)] max-h-[calc(100vh-86px)] overflow-y-auto scrollbar-hide flex flex-col grow-1"
+        >
+          <DetailLink {id} />
+        </div>
+      </div>
+    </div>
+  </div>
 </RouteGuard>
