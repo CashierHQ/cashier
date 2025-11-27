aa8fe9144071d3601a680ce11b72a3159cef729f
Maksym Osovitnii <maksym.osovitnii@neptuness.com>
2025-11-27 15:19:16 +0200

    Refactor link route layouts to enhance structure and consistency. Updated +layout.svelte, +page.svelte in create and detail routes to include AppHeader and streamline rendering logic within RouteGuard, ensuring a cohesive user experience across link management pages.

--- src/cashier_frontend_new/src/routes/link/create/[id]/+page.svelte
+++ src/cashier_frontend_new/src/routes/link/create/[id]/+page.svelte
@@ -4,6 +4,7 @@
   import { page } from "$app/state";
   import { onMount } from "svelte";
   import CreateLink from "$modules/creationLink/pages/create.svelte";
+  import AppHeader from "$modules/shared/components/AppHeader.svelte";
   import RouteGuard from "$modules/shared/components/guards/RouteGuard.svelte";
   import { GuardType } from "$modules/shared/components/guards/types";
   import { appHeaderStore } from "$modules/shared/state/appHeaderStore.svelte";
@@ -41,27 +42,41 @@
   });
 </script>
 
-<div class="w-full grow-1 flex flex-col">
-  <RouteGuard
-    guards={[
-      { type: GuardType.AUTH },
-      { type: GuardType.VALID_LINK, redirectTo: "/links" },
-      { type: GuardType.LINK_OWNER },
-      {
-        type: GuardType.LINK_STATE,
-        allowedStates: [
-          LinkStep.CHOOSE_TYPE,
-          LinkStep.ADD_ASSET,
-          LinkStep.PREVIEW,
-          LinkStep.CREATED,
-        ],
-      },
-    ]}
-    tempLinkId={id}
-  >
-    {@const context = getRouteGuardContext()}
-    {#if context.linkCreationStore}
-      <CreateLink linkStore={context.linkCreationStore} />
-    {/if}
-  </RouteGuard>
-</div>
+<RouteGuard
+  guards={[
+    { type: GuardType.AUTH },
+    { type: GuardType.VALID_LINK, redirectTo: "/links" },
+    { type: GuardType.LINK_OWNER },
+    {
+      type: GuardType.LINK_STATE,
+      allowedStates: [
+        LinkStep.CHOOSE_TYPE,
+        LinkStep.ADD_ASSET,
+        LinkStep.PREVIEW,
+        LinkStep.CREATED,
+      ],
+    },
+  ]}
+  tempLinkId={id}
+>
+  <div class="flex flex-col min-h-screen sm:bg-lightgreen bg-white">
+    <AppHeader isCreateOrEditPage={true} />
+
+    <div class="flex-1 sm:py-4 pb-2 flex items-center justify-center flex-col">
+      <div
+        class="w-full sm:max-w-[600px] max-w-full sm:p-8 px-4 grow-1 bg-white sm:rounded-xl flex flex-col h-full"
+      >
+        <div
+          class="sm:max-h-[calc(100vh-156px)] max-h-[calc(100vh-86px)] overflow-y-auto scrollbar-hide flex flex-col grow-1"
+        >
+          <div class="w-full grow-1 flex flex-col">
+            {@const context = getRouteGuardContext()}
+            {#if context.linkCreationStore}
+              <CreateLink linkStore={context.linkCreationStore} />
+            {/if}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</RouteGuard>
