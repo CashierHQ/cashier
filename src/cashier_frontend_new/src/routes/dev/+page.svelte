63237d461f04c1902ef3c315ed3a02b384c3ce33
Maksym Osovitnii <maksym.osovitnii@neptuness.com>
2025-11-28 11:13:44 +0200

    Remove unused +page.svelte files from dev and props routes, simplifying the codebase and eliminating unnecessary components. This change contributes to improved maintainability and clarity in the routing structure.

--- src/cashier_frontend_new/src/routes/dev/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import BuildInfo from "$modules/dev/components/buildInfo.svelte";
-  import RouteGuard from "$modules/shared/components/guards/RouteGuard.svelte";
-  import { GuardType } from "$modules/shared/types/guards";
-</script>
-
-<RouteGuard guards={[{ type: GuardType.AUTH }]}>
-  <BuildInfo />
-</RouteGuard>
