0234df71c4ec8f727c71ff96702517dfae350954
barchitect <tbng84@gmail.com>
2025-12-18 14:58:10 +0700

    ip blocking flag

--- src/cashier_frontend_new/src/modules/guard/components/ProtectedIP.svelte
+++ src/cashier_frontend_new/src/modules/guard/components/ProtectedIP.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { PROTECTED_IP_BLOCKING_ENABLE } from '$modules/guard/constants';
   import { queryUserCountryLocation } from '$modules/guard/services/ip_resolver';
   import { onMount, type Snippet } from "svelte";
   import ProtectionProcessingState from "./ProtectionProcessingState.svelte";
@@ -9,13 +10,22 @@
     children: Snippet;
   } = $props();
 
-
   let shouldShow = $state(false);
+  const BLACK_LISTED_COUNTRY_CODES = ['US'];
 
   onMount(async () => {
     // Resolve user country location
     const countryCode = await queryUserCountryLocation();
     console.log("User country code:", countryCode);
+
+    if (PROTECTED_IP_BLOCKING_ENABLE && countryCode && BLACK_LISTED_COUNTRY_CODES.includes(countryCode)) {
+      // Block access for blacklisted countries
+      shouldShow = false;
+
+      alert("Access to this application is restricted in your region.");
+      return;
+    }
+    
     shouldShow = true;
   });
 </script>
