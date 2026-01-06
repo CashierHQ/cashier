667c63853fa7c8471e8be35919de0ac76c7126fe
barchitect <tbng84@gmail.com>
2026-01-06 23:43:28 +0700

    user_add_nft IT

--- src/token_storage/src/api/nft.rs
+++ src/token_storage/src/api/nft.rs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License (see LICENSE file in the project root)
 
 use crate::api::state::get_state;
+use cashier_common::guard::is_not_anonymous;
 use ic_cdk::{api::msg_caller, query, update};
 use token_storage_types::{
     dto::nft::{AddUserNftInput, GetUserNftInput, NftDto, UserNftDto},
@@ -13,8 +14,8 @@ use token_storage_types::{
 /// * `input` - The input containing the NFT to be added
 /// # Returns
 /// * `UserNftDto` - The added NFT with user information
-#[update]
-pub async fn add_user_nft(input: AddUserNftInput) -> Result<UserNftDto, CanisterError> {
+#[update(guard = "is_not_anonymous")]
+pub async fn user_add_nft(input: AddUserNftInput) -> Result<UserNftDto, CanisterError> {
     let mut state = get_state();
     let user = msg_caller();
     state.user_nft.add_nft(user, input.nft).await
@@ -25,8 +26,8 @@ pub async fn add_user_nft(input: AddUserNftInput) -> Result<UserNftDto, Canister
 /// * `input` - The input containing pagination parameters
 /// # Returns
 /// * `Vec<NftDto>` - List of NFTs owned by the user
-#[query]
-pub fn get_user_nfts(input: GetUserNftInput) -> Vec<NftDto> {
+#[query(guard = "is_not_anonymous")]
+pub fn user_get_nfts(input: GetUserNftInput) -> Vec<NftDto> {
     let state = get_state();
     let user = msg_caller();
     state.user_nft.get_nfts(&user, input.start, input.limit)
