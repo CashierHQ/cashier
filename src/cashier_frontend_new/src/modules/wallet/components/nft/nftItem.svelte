423282a9b2e516525c1c0a6975f66b9646e1efb1
barchitect <tbng84@gmail.com>
2026-01-08 21:15:16 +0700

    nft tab

---
+++ src/cashier_frontend_new/src/modules/wallet/components/nft/nftItem.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { NFT } from '$modules/wallet/types/nft';
+
+
+  interface Props {
+    item: NFT;
+    onSelect: (address: string) => void;
+    failedImageLoads: Set<string>;
+    onImageError: (address: string) => void;
+  }
+
+  let {
+    item,
+    onSelect,
+    failedImageLoads,
+    onImageError,
+  }: Props = $props();
+
+</script>
+<div>
+  
+</div>
