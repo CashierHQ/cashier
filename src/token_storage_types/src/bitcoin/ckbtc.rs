d19f6053dc1f45dba7a1bca96969ffe200198355
barchitect <tbng84@gmail.com>
2026-01-15 08:58:25 +0700

    btc address

---
+++ src/token_storage_types/src/bitcoin/ckbtc.rs
@@ -0,0 +1,29 @@
+// Copyright (c) 2025 Cashier Protocol Labs
+// Licensed under the MIT License (see LICENSE file in the project root)
+
+use candid::CandidType;
+use cashier_macros::storable;
+use ic_mple_structures::Codec;
+
+#[derive(Clone, Debug, CandidType, PartialEq, Eq, Hash)]
+#[storable]
+pub struct BtcAddress {
+    pub address: Vec<u8>,
+}
+
+#[storable]
+pub enum BtcAddressCodec {
+    V1(BtcAddress),
+}
+
+impl Codec<BtcAddress> for BtcAddressCodec {
+    fn decode(source: Self) -> BtcAddress {
+        match source {
+            BtcAddressCodec::V1(address) => address,
+        }
+    }
+
+    fn encode(dest: BtcAddress) -> Self {
+        BtcAddressCodec::V1(dest)
+    }
+}
