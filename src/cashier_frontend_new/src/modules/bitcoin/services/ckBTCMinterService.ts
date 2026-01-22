306b4d3ed4e101a0555c60caff2049c942171e46
barchitect <tbng84@gmail.com>
2026-01-22 11:53:33 +0700

    ckbtc minter info

--- src/cashier_frontend_new/src/modules/bitcoin/services/ckBTCMinterService.ts
+++ src/cashier_frontend_new/src/modules/bitcoin/services/ckBTCMinterService.ts
@@ -1,7 +1,10 @@
 import * as ckBTCMinter from "$lib/generated/ckbtc_minter/ckbtc_minter.did";
 import { authState } from "$modules/auth/state/auth.svelte";
 import { CKBTC_MINTER_CANISTER_ID } from "$modules/bitcoin/constants";
-import { type WithdrawalFee } from "$modules/bitcoin/types/ckbtc_minter";
+import {
+  type MinterInfo,
+  type WithdrawalFee,
+} from "$modules/bitcoin/types/ckbtc_minter";
 
 export class CkBTCMinterService {
   #canisterId: string;
@@ -38,6 +41,14 @@ export class CkBTCMinterService {
       amount: [amount],
     });
     return withdrawalFee;
+  }
+
+  async getMinterInfo(): Promise<MinterInfo> {
+    const actor = this.#getActor();
+    if (!actor) {
+      throw new Error("User is not authenticated");
+    }
+    return actor.get_minter_info();
   }
 }
 
--- src/cashier_frontend_new/src/modules/bitcoin/types/ckbtc_minter.ts
+++ src/cashier_frontend_new/src/modules/bitcoin/types/ckbtc_minter.ts
@@ -2,3 +2,9 @@ export type WithdrawalFee = {
   minter_fee: bigint;
   bitcoin_fee: bigint;
 };
+
+export type MinterInfo = {
+  kyt_fee: bigint;
+  retrieve_btc_min_amount: bigint;
+  min_confirmations: number;
+};
