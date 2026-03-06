aafc2ffcf9b8e5c3206196235c509d41f485b9ab
Long Tran <57053685+baolongt@users.noreply.github.com>
2026-03-07 00:38:57 +0700

    [Ticket #869cau856] Clean up other's code (#734)

--- src/cashier_frontend/src/utils/helpers/date/pretty.ts
+++ src/cashier_frontend/src/utils/helpers/date/pretty.ts
@@ -1,10 +1,12 @@
 // Copyright (c) 2025 Cashier Protocol Labs
 // Licensed under the MIT License (see LICENSE file in the project root)
 
+const DATE_FORMATTER = new Intl.DateTimeFormat("en-US", {
+  month: "short",
+  day: "numeric",
+  year: "numeric",
+});
+
 export function formatDate(date: Date) {
-  return date.toLocaleDateString("en-US", {
-    month: "short",
-    day: "numeric",
-    year: "numeric",
-  });
+  return DATE_FORMATTER.format(date);
 }
