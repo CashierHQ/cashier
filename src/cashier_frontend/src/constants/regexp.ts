2b4b8388fe2338b8287eb286dc1e44a5b1a3af6a
Long Tran <57053685+baolongt@users.noreply.github.com>
2026-03-12 23:00:35 +0700

    chore: remove deprecated cashier_frontend folder and update references
    Old React/Vite frontend fully replaced by cashier_frontend_new (SvelteKit).
    Updated dfx.json, canister_ids.json, CI/CD workflows, and docs to point
    to cashier_frontend_new. No cross-dependencies existed.

--- src/cashier_frontend/src/constants/regexp.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2025 Cashier Protocol Labs
-// Licensed under the MIT License (see LICENSE file in the project root)
-
-export const CANISTER_ID_REGEX =
-  /^[a-z0-9]{5}-?[a-z0-9]{5}-?[a-z0-9]{5}-?[a-z0-9]{5}-?[a-z0-9]{3}-?([a-z0-9]{2})?$/i;
--- src/cashier_frontend/src/types/transaction-type.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025 Cashier Protocol Labs
-// Licensed under the MIT License (see LICENSE file in the project root)
-
-export enum TransactionType {
-  Send,
-  Receive,
-  // Other
-}
