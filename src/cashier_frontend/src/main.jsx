983a60ad311bfbe2abc5421e824542fc24b073df
Long Tran <57053685+baolongt@users.noreply.github.com>
2026-03-13 12:27:27 +0700

    [Ticket #869cau856] Clean up old frontend folder (#736)

--- src/cashier_frontend/src/main.jsx
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025 Cashier Protocol Labs
-// Licensed under the MIT License (see LICENSE file in the project root)
-
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "@radix-ui/themes/styles.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- src/cashier_frontend/src/services/token_price/index.ts
+++
@@ -1,7 +0,0 @@
-import { Result } from "ts-results";
-
-/// Interface for token price clients
-export interface TokenPriceClient {
-  //// @returns An array of pairs (token_address, token_price)
-  getTokenPrices(): Promise<Result<Record<string, number>, Error>>;
-}
