0956da02c3947264ec443a023ed047d5b65851dc
Maksym Osovitnii <maksym.osovitnii@neptuness.com>
2026-04-23 16:38:26 +0300

    Remove ProtectedUserState and ProtectedValidLink test files as part of code cleanup; no longer needed for current implementation.

---
+++ src/cashier_frontend_new/src/modules/guard/components/ProtectedValidLinkTestHost.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import ProtectedValidLink from "./ProtectedValidLink.svelte";
+
+  let { redirectTo }: { redirectTo?: string } = $props();
+</script>
+
+<ProtectedValidLink {redirectTo}>
+  <div data-testid="child">Child content</div>
+</ProtectedValidLink>
