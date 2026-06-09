b58ff757c0c0cde8cf8312bc5b48a1fed4c4a35c
SebastianRubina <sebastian.rubina@icloud.com>
2026-06-09 19:57:11 -0400

    Fix timezone-stable date formatting tests

--- src/cashier_frontend_new/src/modules/routing/routeScreen.spec.ts
+++ src/cashier_frontend_new/src/modules/routing/routeScreen.spec.ts
@@ -11,6 +11,8 @@ describe("routeScreen", () => {
       ["/link/detail/link-1", { area: "detail", linkId: "link-1" }],
       ["/link/public-1", { area: "userLanding", linkId: "public-1" }],
       ["/link/public-1/use", { area: "userUse", linkId: "public-1" }],
+      ["/link/create", { area: "userLanding", linkId: "create" }],
+      ["/link/detail", { area: "userLanding", linkId: "detail" }],
     ] as const)("maps %s to the expected route match", (pathname, expected) => {
       expect(parseRoute(pathname)).toEqual(expected);
     });
@@ -18,8 +20,6 @@ describe("routeScreen", () => {
     it.each([
       "/unknown",
       "/link",
-      "/link/create",
-      "/link/detail",
       "/link/public-1/unknown",
       "/link/create/draft-1/extra",
     ])("maps %s to unknown", (pathname) => {
